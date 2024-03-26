79b0023471832d074c47159483447810d9280c89
enesalagoz1212 <118561378+enesalagoz1212@users.noreply.github.com>
2024-03-26 07:31:42 +0300

    grid and area in css

--- Ogrenciler/EnesAlagoz/Uygulamalar/24.03.2024/script.js
+++ Ogrenciler/EnesAlagoz/Uygulamalar/24.03.2024/script.js
@@ -16,11 +16,11 @@ let foodX;
 let foodY;
 let score = 0;
 let snake=[
-    {x:unitSize * 4, y:0},
-    {x:unitSize * 3, y:0},
-    {x:unitSize * 2, y:0},
-    {x:unitSize, y:0},
-    {x:0, y:0}
+    {x:25, y:0},
+    {x:25, y:0},
+    {x:25, y:0},
+    {x:25, y:0},
+    {x:25, y:0}
 ];
 
 window.addEventListener("keydown", changeDirection);
@@ -71,7 +71,7 @@ function drawFood(){
 function moveSnake(){
     const head={x: snake[0].x + xVelocity,
                 y: snake[0].y + yVelocity};
-    snake.unshift(head);
+   snake.unshift(head);
     // if food is eaten
     if(snake[0].x == foodX && snake[0].y == foodY){
         score+=1;
@@ -80,7 +80,8 @@ function moveSnake(){
     }
     else{
         snake.pop();
-    }            
+    }    
+       
 }
 function drawSnake(){
     ctx.fillStyle=snakeColor;
@@ -157,11 +158,11 @@ function resetGame(){
     xVelocity = unitSize;
     yVelocity= 0;
     snake=[
-        {x:unitSize * 4, y:0},
-        {x:unitSize * 3, y:0},
-        {x:unitSize * 2, y:0},
-        {x:unitSize, y:0},
-        {x:0, y:0}
+        {x:25, y:0},
+        {x:25, y:0},
+        {x:25, y:0},
+        {x:25, y:0},
+        {x:25, y:0}
     ];
     gameStart();
 }
