67f985c5acf53c06981ff3ea7ff62497ad91b8ad
Veli Dogan <velidogan120@gmail.com>
2024-04-26 21:06:20 +0300

    Last Update

--- Ogrenciler/velidogan/Practice/Javascript/app.js
+++ Ogrenciler/velidogan/Practice/Javascript/app.js
@@ -6,16 +6,20 @@ let score = 0;
 
 for(let i = 0;i > -1;i++){
     let tahminSayi = Number(prompt("1 ile 20 arasında sayı giriniz."));
-    if(sayi == tahminSayi){
-        let puan = calculateScore(tahmin);
-        alert("tebrikler " + tahmin + " tahminde bildiniz. Skorunuz : " + puan);
-        break;
-    }else if(tahminSayi > sayi){
-        alert("Aşağı gidiniz.");
-    }else if(tahminSayi < sayi){
-        alert("Yukarı gidiniz.");
-    }
-    tahmin++;
+    if(tahminSayi == " "){
+        alert("geçersiz karakter");
+    }else{
+        if(sayi == tahminSayi){
+            let puan = calculateScore(tahmin);
+            alert("tebrikler " + tahmin + " tahminde bildiniz. Skorunuz : " + puan);
+            break;
+        }else if(tahminSayi > sayi){
+            alert("Aşağı gidiniz.");
+        }else if(tahminSayi < sayi){
+            alert("Yukarı gidiniz.");
+        }
+        tahmin++;
+    }    
 }
 
 function calculateScore(tahminSayi) {
