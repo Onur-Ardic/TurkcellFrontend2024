3367e068c058f69bb96d15ef09536215a5e56282
aleynaertok <ertok74@gmail.com>
2024-04-28 17:29:59 +0300

    file arrangement made

--- Ogrenciler/Aleyna/feLessons/ders11-js-numberGuessingGame/app.js
+++ Ogrenciler/Aleyna/feLessons/ders11-js-numberGuessingGame/app.js
@@ -1,28 +1,28 @@
 let sayi = Math.floor(Math.random() * 20);
 let tahmin = 1;
-let maxScore = 100; 
-let minScore = 10; 
+let maxScore = 100;
+let minScore = 10;
 let score = 0;
 
-for(let i = 0;i > -1;i++){
-    let tahminSayi = Number(prompt("1 ile 20 arasında sayı giriniz."));
-    if(sayi == tahminSayi){
-        let puan = calculateScore(tahmin);
-        alert("tebrikler " + tahmin + " tahminde bildiniz. Skorunuz : " + puan);
-        break;
-    }else if(tahminSayi > sayi){
-        alert("Aşağı gidiniz.");
-    }else if(tahminSayi < sayi){
-        alert("Yukarı gidiniz.");
-    }
-    tahmin++;
+for (let i = 0; i > -1; i++) {
+  let tahminSayi = Number(prompt("1 ile 20 arasında bir sayı giriniz."));
+  if (sayi == tahminSayi) {
+    let puan = calculateScore(tahmin);
+    alert("Tebrikler " + tahmin + " tahminde bildiniz. Skorunuz : " + puan);
+    break;
+  } else if (tahminSayi > sayi) {
+    alert("Daha düşük bir tahminde bulunun.");
+  } else if (tahminSayi < sayi) {
+    alert("Daha yüksek bir tahminde bulunun.");
+  }
+  tahmin++;
 }
 
 function calculateScore(tahminSayi) {
-    if(tahminSayi > 90){
-        score = minScore;
-    }else{
-        score = maxScore - tahminSayi;
-    }
-    return score;
+  if (tahminSayi > 90) {
+    score = minScore;
+  } else {
+    score = maxScore - tahminSayi;
+  }
+  return score;
 }
