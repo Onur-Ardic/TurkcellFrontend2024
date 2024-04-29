2ba35544a25f387e7121189d86fd51bbf7164608
Onur Ardıç <onurardc@outlook.com>
2024-04-29 20:31:25 +0300

    bug fixed

--- Ogrenciler/OnurArdic/Takım Çalışmaları/ders12/main.js
+++ Ogrenciler/OnurArdic/Takım Çalışmaları/ders12/main.js
@@ -1,17 +1,15 @@
-const value = prompt('Lütfen bir sayı giriniz')
+const value = Number(prompt('Lütfen bir sayı giriniz'))
 function process() {
   alert(`
-  
   1 - Faktöriyel Hesaplama
   2 - Tek Çift Hesaplama
   3 - Süper Sayı Hesaplama
   4 - Asal Sayı Hesaplama
-
   `)
 
   const switchCase = Number(prompt('Lütfen bir işlem seçiniz'))
 
-  return toString(switchCase)
+  return switchCase
 }
 
 const tercih = process()
@@ -19,7 +17,7 @@ const tercih = process()
 switch (tercih) {
   case 1:
     let result = 1
-    for (let i = 1; i <= value; i++) {
+    for (let i = 1; i < value; i++) {
       result *= i
     }
 
@@ -27,44 +25,40 @@ switch (tercih) {
 
     break
   case 2:
-    const copy = () => {
-      for (let i = 1; i <= value; i++) {
-        if (i % 2 === 0) {
-          alert(`${value} Sayı çifttir`)
-        } else {
-          alert(`${value} Sayı tektir`)
-        }
+    for (let i = 0; i < value; i++) {
+      if (value % 2 == 0) {
+        alert(`${value} Sayı çifttir`)
+        break
+      } else {
+        alert(`${value} Sayı tektir`)
+        break
       }
     }
     break
 
   case 3:
-    const supper = () => {
-      let sayiToplam = 0
-      for (let i = 1; i < value; i++) {
-        if (value % i === 0) {
-          sayiToplam = +i
-        }
+    let sayiToplam = 0
+    for (let i = 1; i < value; i++) {
+      if (value % i == 0) {
+        sayiToplam += i
       }
-      if (value == sayiToplam) {
-        alert(`${value} Sayı süper sayıdır`)
-      } else {
-        alert(`${value} Sayı süper sayı değildir`)
-      }
+    }
+    if (value == sayiToplam) {
+      alert(`${value} Sayı süper sayıdır`)
+    } else {
+      alert(`${value} Sayı süper sayı değildir`)
     }
     break
 
   case 4:
-    const asal = () => {
-      for (let i = 2; i < value; i++) {
-        if (value % i === 0) {
-          alert(`${value} Sayı asal değildir`)
-        } else {
-          alert(`${value} Sayı asaldır`)
-        }
+    for (let i = 2; i < value; i++) {
+      if (value % i === 0) {
+        alert(`${value} Sayı asal değildir`)
+        break
+      } else {
+        alert(`${value} Sayı asaldır`)
+        break
       }
     }
     break
-
-  default:
 }
