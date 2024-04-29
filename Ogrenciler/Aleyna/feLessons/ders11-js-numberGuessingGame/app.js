5e01bea654e968ecc44bd03f706d4f88bfc0387c
aleynaertok <ertok74@gmail.com>
2024-04-29 19:23:51 +0300

    lesson12 added

--- Ogrenciler/Aleyna/feLessons/ders11-js-numberGuessingGame/app.js
+++ Ogrenciler/Aleyna/feLessons/ders11-js-numberGuessingGame/app.js
@@ -1,11 +1,11 @@
-let sayi = Math.floor(Math.random() * 20);
+let sayi = Math.floor(Math.random() * 100);
 let tahmin = 1;
 let maxScore = 100;
 let minScore = 10;
 let score = 0;
 
 for (let i = 0; i > -1; i++) {
-  let tahminSayi = Number(prompt("1 ile 20 arasında bir sayı giriniz."));
+  let tahminSayi = Number(prompt("1 ile 100 arasında bir sayı giriniz."));
   if (sayi == tahminSayi) {
     let puan = calculateScore(tahmin);
     alert("Tebrikler " + tahmin + " tahminde bildiniz. Skorunuz : " + puan);
