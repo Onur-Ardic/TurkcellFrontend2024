dba7e06eb974a320da900bcd14ce3057ac0cc2a1
Alpay Özer <alpayozer7@gmail.com>
2024-04-29 20:55:33 +0300

    lesson 12

---
+++ Ogrenciler/alpayozer/Lessons/Lesson-12-JsFunctions/app.js
@@ -0,0 +1,56 @@
+// function hello() {
+//   console.log("hello world");
+// }
+// hello();
+
+// function user(name, age) {
+//   console.log(`Name: ${name} Age: ${age}`);
+// }
+// user("Alpay", 25);
+
+// function user(name = "Bilgi yok", age = "Bilgi yok") {
+//   console.log(`Name: ${name} Age: ${age}`);
+// }
+// user();
+
+//return value
+// function square(x) {
+//   return x * x;
+// }
+// const result = square(5) * 3;
+// console.log(result);
+
+//Arrow Functions
+// const kare = (x) => x * x;
+// console.log(kare(3));
+
+//Do While - While
+// let i = 0;
+// while (i < 5) {
+//   console.log(i);
+//   i++;
+// }
+
+// let i = 0;
+// while (i < 10) {
+//   if (i === 3) {
+//     i++;
+//     continue;
+//   }
+//   console.log(i);
+//   i++;
+// }
+
+// let i = 0;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i < 10);
+
+// Array map()
+// const langs = ["Python", "Java", "C++", "Javascript"];
+
+// langs.forEach((lang) => {
+//   console.log(lang);
+// });
+// langs.map((lang) => console.log(lang));
