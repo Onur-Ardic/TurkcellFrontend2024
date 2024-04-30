bef2f1b8fda54f11b1324d4259a4cf76e360dbcd
Barış Erkuş <1bariserkus@gmail.com>
2024-04-30 09:07:35 +0300

    simplified

--- Ogrenciler/BarisErkus/Lessons/lesson-12-js/app.js
+++ Ogrenciler/BarisErkus/Lessons/lesson-12-js/app.js
@@ -1,33 +1,16 @@
 const isValid = value => isNaN(value) || value < 0 ? false : true;
 const tekcift = sayi => sayi % 2 == 0 ? 'Sayı Çift' : 'Sayı Tek'
-const faktoriyel = sayi => {
-    if (sayi == 0 || sayi == 1) {
-        return 1;
-    }
-    return sayi * faktoriyel(sayi - 1);
-}
+const faktoriyel = sayi => (sayi == 0 || sayi == 1) ? 1 : sayi * faktoriyel(sayi - 1);
 const isPrime = sayi => {
     if (sayi <= 1) return `Sayı Asal Değil: ${sayi}`;
-    for (let i = 2; i <= sayi / 2; i++) {
-        if (sayi % i == 0) {
-            return `Sayı Asal Değil: ${sayi}`;
-        }
-        continue;
-    }
+    for (let i = 2; i <= sayi / 2; i++) if (sayi % i == 0) return `Sayı Asal Değil: ${sayi}`;
     return `Sayı Asal: ${sayi}`
 }
 const isSuper = sayi => {
     let result = 1;
     if (sayi <= 1) return 'Bu bir süper sayı değil.'
-    for (let i = 2; i <= sayi / 2; i++) {
-        if (sayi % i == 0) {
-            result += i;
-        }
-    }
-    if (result == sayi) {
-        return 'Bu bir süper sayı.'
-    }
-    return 'Bu bir süper sayı değil.'
+    for (let i = 2; i <= sayi / 2; i++) if (sayi % i == 0) result += i;
+    return result == sayi ? 'Bu bir süper sayı.' : 'Bu bir süper sayı değil.'
 }
 while (true) {
     const userValue = prompt("Lütfen bir sayı giriniz: ");
@@ -43,7 +26,7 @@ while (true) {
 
     switch (operationValue) {
         case "1":
-            alert(`Faktöriyelin Sonucu: ${faktoriyel(userValue)}`);
+            alert(`Faktöriyelin Sonucu (${userValue}): ${faktoriyel(userValue)}`);
             break;
         case "2":
             alert(isPrime(userValue));
@@ -54,9 +37,8 @@ while (true) {
         case "4":
             alert(isSuper(userValue));
             break;
-        case "0":
-            window.close()
         default:
-            alert('Lütfen geçerli bir sayı giriniz.');
+            alert('Lütfen geçerli bir işlem seçiniz.');
+            break;
     }
 }
