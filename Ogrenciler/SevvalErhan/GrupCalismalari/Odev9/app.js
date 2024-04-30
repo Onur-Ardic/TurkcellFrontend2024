2f0f4d680a4f156a5c184d0455f2af2297557fae
ŞEVVAL ERHAN <sevvalerhan8@gmail.com>
2024-04-30 21:27:54 +0300

    hesap makinesi

---
+++ Ogrenciler/SevvalErhan/GrupCalismalari/Odev9/app.js
@@ -0,0 +1,50 @@
+const toplama = (numberOne , numberTwo) => numberOne + numberTwo;
+const cikarma = (numberOne , numberTwo) => numberOne - numberTwo;
+const bölme = (numberOne , numberTwo) => numberOne / numberTwo;
+const carpma = (numberOne , numberTwo) => numberOne * numberTwo;
+const tercih = (cevap) => {
+    let cevap = prompt("Devam etmek istiyor musunuz ?(E/H)")
+    if (cevap.toUppercase() == "H") {
+   return
+    }
+    else{
+       window.location.reload()
+    }
+};
+
+while (true) {
+    const numberOne = Number(prompt ("Birinci sayıyı giriniz"));
+    const numberTwo = Number(prompt ("İkinci sayıyı giriniz"));
+    const process = Number (prompt(`
+  
+        1 - Toplama 
+        2 - Çıkarma
+        3 - Bölme
+        4 - Çarpma
+
+      `)
+    )
+    switch (process) {
+        case 1:
+            alert(toplama (numberOne , numberTwo))
+        
+          break
+        case 2:
+            alert(cikarma (numberOne , numberTwo))
+          break
+      
+        case 3:
+            alert(bölme (numberOne , numberTwo))
+          break
+      
+        case 4:
+            alert(carpma (numberOne , numberTwo))
+          break
+      
+        default: tercih()
+        
+      }
+}
+
+
+
