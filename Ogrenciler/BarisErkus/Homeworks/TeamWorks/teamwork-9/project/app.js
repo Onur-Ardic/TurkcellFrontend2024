b6e58672335b9c2a445fe47fdfa8737620a87e49
Barış Erkuş <1bariserkus@gmail.com>
2024-05-01 14:22:34 +0300

    js odev son hali

--- Ogrenciler/BarisErkus/Homeworks/TeamWorks/teamwork-9/project/app.js
+++ Ogrenciler/BarisErkus/Homeworks/TeamWorks/teamwork-9/project/app.js
@@ -2,10 +2,13 @@ const sum = (number1, number2) => number1 + number2;
 const subs = (number1, number2) => number1 - number2;
 const multiple = (number1, number2) => number1 * number2;
 const divide = (number1, number2) => number1 / number2;
+const mod = (number1, number2) => number1 % number2;
+const pow = (number1, number2) => number1 ** number2;
 
 while (true) {
     const input = prompt(`Lütfen işleminizi boşluk bırakarak giriniz: 
-(Örnek: 3 + 1)`);
+Tüm işlemlerin sonuçlarını görmek için (.) 'Nokta' işaretini kullanınız.  
+(Örnek: 3 . 1)`);
     let [userValue1, operationValue, userValue2] = input.split(" ");
     userValue1 = Number(userValue1);
     userValue2 = Number(userValue2);
@@ -23,16 +26,29 @@ while (true) {
         case "/":
             alert(`Bölme İşlemi: ${divide(userValue1, userValue2)} `);
             break;
+        case "%":
+            alert(`Mod İşlemi: ${mod(userValue1, userValue2)}`);
+            break;
+        case "**":
+            alert(`Üs Alma İşlemi: ${pow(userValue1, userValue2)}`);
+            break;
+        case ".": 
+            alert(`Toplam: ${sum(userValue1, userValue2)}
+Fark: ${subs(userValue1, userValue2)}
+Çarpım: ${multiple(userValue1, userValue2)}
+Bölüm: ${divide(userValue1, userValue2)}
+Mod: ${mod(userValue1, userValue2)}
+Üs: ${pow(userValue1, userValue2)}`)
+            break;
         default:
             alert('Lütfen geçerli bir işlem seçiniz.');
             break;
     }
 
-    const again = prompt(`Uygulamadan çıkmak için 'H''yi tuşlayınız. 
+    const again = prompt(`Uygulamadan çıkmak için 'H'yi tuşlayınız. 
 Devam etmek için herhangi bir tuşa basınız.`);
     if (again.toUpperCase() == 'H') {
         window.close()
         break;
     }
 }
-
