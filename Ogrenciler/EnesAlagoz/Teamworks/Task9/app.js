840d5da27f550e412895d2dcd677912680a474c1
enesalagoz1212 <118561378+enesalagoz1212@users.noreply.github.com>
2024-05-03 17:51:25 +0300

    update

--- Ogrenciler/EnesAlagoz/Teamworks/Task9/app.js
+++ Ogrenciler/EnesAlagoz/Teamworks/Task9/app.js
@@ -15,9 +15,10 @@ function bol(num1, num2) {
 }
 
 while (true) {
-  const islem = Number(prompt("Yapmak istediğiniz işlemi seçiniz:\n 1-Toplama\n 2-Çıkarma\n 3-Çarpma\n 4-Bölme"));
+  const hesapMakinesi = alert(" 1-Toplama\n 2-Çıkarma\n 3-Çarpma\n 4-Bölme");
   const num1 = Number(prompt("Birinci sayıyı giriniz"));
   const num2 = Number(prompt("İkinci sayıyı giriniz"));
+  const islem = Number(prompt("Yapmak istediğiniz işlemi seçiniz:\n 1-Toplama\n 2-Çıkarma\n 3-Çarpma\n 4-Bölme"));
   switch (islem) {
     case 1:
       alert(topla(num1, num2));
