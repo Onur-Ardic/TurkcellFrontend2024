2b39dde6318087a3ac794943bea3e481d1c3b12e
Umutcan Kocamis <umutceaan@gmail.com>
2024-05-04 21:58:42 +0300

    js card odev bitti

---
+++ Ogrenciler/UmutcanKocamis/GrupOdevleri/jsFormOdev/app.js
@@ -0,0 +1,43 @@
+const info = {
+  name: "",
+  surname: "",
+  city: "",
+  age: "",
+};
+const nameInput = document.getElementById("nameInput");
+const surnameInput = document.getElementById("surnameInput");
+const cityInput = document.getElementById("cityInput");
+const ageInput = document.getElementById("ageInput");
+
+const name = document.getElementById("name");
+const surname = document.getElementById("surname");
+const city = document.getElementById("city");
+const age = document.getElementById("age");
+
+const kaydet = document
+  .getElementById("submit")
+  .addEventListener("click", kaydetme);
+
+const temizle = document
+  .getElementById("clear")
+  .addEventListener("click", temizleme);
+
+function kaydetme() {
+  //Objede tutmak için
+  info.name = nameInput.value;
+  info.surname = surnameInput.value;
+  info.city = cityInput.value;
+  info.age = ageInput.value;
+  //Ekrana göstermek için
+  name.innerHTML = info.name;
+  surname.innerHTML = info.surname;
+  city.innerHTML = info.city;
+  age.innerHTML = info.age;
+}
+
+function temizleme() {
+  nameInput.value = "";
+  surnameInput.value = "";
+  cityInput.value = "";
+  ageInput.value = "";
+}
