9a1067ec3409a9ab07ad518e2cd9cff310ad4e62
sarvanss <thesarvanss@gmail.com>
2024-05-05 18:59:18 +0300

    ödev

--- Ogrenciler/Muhammed Saka/DersVeProje/ders13/script.js
+++ Ogrenciler/Muhammed Saka/DersVeProje/ders13/script.js
@@ -1,11 +1,50 @@
-function bilgiGoster() {
-    var ad = document.getElementById("ad").value;
-    var soyad = document.getElementById("soyad").value;
-    var yas = document.getElementById("yas").value;
-    var memleket = document.getElementById("memleket").value;
-
-    console.log("Ad: " + ad);
-    console.log("Soyad: " + soyad);
-    console.log("Yaş: " + yas);
-    console.log("Memleket: " + memleket);
+const name = document.getElementById('name');
+const surname = document.getElementById('surname');
+const age = document.getElementById('age');
+const address = document.getElementById('address');
+const savedDataContainer = document.getElementById('savedData');
+const newData = { 
+    name,
+    surname,
+    age,
+    address
 }
+function saveData() { 
+    
+    newData.name = name.value;
+    newData.surname = surname.value;
+    newData.age = age.value;    
+    newData.address = address.value;  
+    
+
+
+    // Kaydedilen verileri göster
+    showSavedData();
+
+    name.value = '';
+    surname.value = '';
+    age.value = '';
+    address.value = '';
+}
+
+function resetForm() {
+    document.getElementById('myForm').reset();
+    formData = [];
+    document.getElementById('savedData').innerHTML = ''; // Kaydedilen verileri temizle
+}
+
+function showSavedData() {
+    savedDataContainer.innerHTML = '';
+
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        card.innerHTML = `
+            <p><strong>Adı:</strong> ${newData.name}</p>
+            <p><strong>Soyadı:</strong> ${newData.surname}</p>
+            <p><strong>Yaşı:</strong> ${newData.age}</p>
+            <p><strong>Adresi:</strong> ${newData.address}</p>
+        `;
+
+        savedDataContainer.appendChild(card);
+    }
