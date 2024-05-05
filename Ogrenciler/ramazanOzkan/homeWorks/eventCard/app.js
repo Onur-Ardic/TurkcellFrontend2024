f0ffe9c2272de27fcf5e8f09e68be90c3b2f10ed
ramazanozkan777 <54441645+ramazanozkan777@users.noreply.github.com>
2024-05-05 08:50:37 +0300

    card-events
    +tüm mekanikler çalışıyor.
    -aynı hesap numarası olunca veya hesap numarası "00..0" durumunda kaydet butonunu inaktif etmiyoruz.

--- Ogrenciler/ramazanOzkan/homeWorks/eventCard/app.js
+++ Ogrenciler/ramazanOzkan/homeWorks/eventCard/app.js
@@ -1,22 +1,26 @@
 const nameInput = document.getElementById("nameInput");
 const nameCard = document.getElementById("nameCard");
-
-function randomNum() {
-    return Math.round(Math.random() * 9 );
-} 
 nameInput.addEventListener("keyup", ()=> {
     nameCard.textContent = nameInput.value.toUpperCase();
 });
 const surnameInput = document.getElementById("surnameInput");
 const surnameCard = document.getElementById("surnameCard");
-
 surnameInput.addEventListener("keyup", ()=> {
     surnameCard.textContent = surnameInput.value.toUpperCase();
 });
-
+function randomNum() {
+    return Math.round(Math.random() * 9 );
+} 
 const cardNumber =document.getElementById("cardNumber");
 const createNumber = document.getElementById("createNumber");
+
+// if(cardNumber=="0000 0000 0000 0000")
+//     {
+//         save.disabled=true;
+//         console.log(cardNumber.textContent)
+//     }
 createNumber.addEventListener("click", ()=>{
+    // save.disabled=false;
     let cardNumberCreate="";
     for (let i = 0; i < 16; i++){
         if(i%4==0){
@@ -79,3 +83,43 @@ clear.addEventListener("click",()=>{
     loc.checked = false;
 }) ;
 
+let idCounter = 1;
+const save = document.getElementById("save");
+const savedCardsContainer = document.getElementById('savedCardsContainer');
+const savedCardsContainer2 = document.getElementById('savedCardsContainer2');
+const header= document.getElementById('saveHeader');
+const clsCards=document.getElementById("clsCards");
+save.addEventListener("click", () => {
+    header.style.display = "block";
+    const creditCard = document.getElementById('creditsCard');
+    const newCreditCard = creditCard.cloneNode(true); // kartın kopyası.
+    newCreditCard.id = "savedCard_" + idCounter;
+    idCounter += 1;
+    newCreditCard.classList.add("mb-3");
+    if (idCounter % 2 == 0) {
+        savedCardsContainer.appendChild(newCreditCard);
+    } else {
+        savedCardsContainer2.appendChild(newCreditCard);
+    }
+    if(idCounter == 7)
+        {
+            save.disabled=true;
+            clsCards.style.display="block";
+        }
+});
+
+clsCards.addEventListener("click",()=>{
+    header.style.display="none";
+    idCounter=1;
+    save.disabled=false;
+    clsCards.style.display="none";
+    // savedCardsContainer.innerHTML = ""; 
+    // savedCardsContainer2.innerHTML = "";
+    // innerhtml kullanmamak için döngü içerisinde tüm elemanları gezerek tek tek sildik.
+    while (savedCardsContainer.firstChild) {
+        savedCardsContainer.removeChild(savedCardsContainer.firstChild);
+    }
+    while (savedCardsContainer2.firstChild) {
+        savedCardsContainer2.removeChild(savedCardsContainer2.firstChild);
+    }
+})
