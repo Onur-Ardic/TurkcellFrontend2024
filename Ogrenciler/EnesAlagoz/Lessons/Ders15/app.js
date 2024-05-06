c13919cb4b4326944bf5dcaa300b8fedb162b9e1
enesalagoz1212 <118561378+enesalagoz1212@users.noreply.github.com>
2024-05-06 16:26:21 +0300

    form added

--- Ogrenciler/EnesAlagoz/Lessons/Ders15/app.js
+++ Ogrenciler/EnesAlagoz/Lessons/Ders15/app.js
@@ -1,14 +1,44 @@
-document.getElementById("person").addEventListener("submit", function (event) {
-  event.preventDefault();
-});
-var firstName = document.getElementById("name").value;
-var lastName = document.getElementById("surname").value;
-var city = document.getElementById("city").value;
-var userInfo = { firstName: firstName, lastName: lastName, city: city };
-console.log("Kullanıcı Bilgileri:", userInfo);
-console.log(userInfo);
+const name = document.getElementById("name");
+const surname = document.getElementById("surname");
+const age = document.getElementById("age");
+const address = document.getElementById("address");
+const savedDataContainer = document.getElementById("savedData");
+const newData = {
+  name,
+  surname,
+  age,
+  address,
+};
+function saveData() {
+  newData.name = name.value;
+  newData.surname = surname.value;
+  newData.age = age.value;
+  newData.address = address.value;
 
-console.log("Ad: " + firstName);
-console.log("Soyad: " + lastName);
+  showSavedData();
 
-console.log("Memleket: " + city);
+  name.value = "";
+  surname.value = "";
+  age.value = "";
+  address.value = "";
+}
+
+function resetForm() {
+  document.getElementById("myForm").reset();
+  formData = [];
+  document.getElementById("savedData").innerHTML = "";
+}
+
+function showSavedData() {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  card.innerHTML = `
+            <h3><strong>Adı:</strong> ${newData.name}</h3>
+            <h3><strong>Soyadı:</strong> ${newData.surname}</h3>
+            <h3><strong>Yaşı:</strong> ${newData.age}</h3>
+            <h3><strong>Adresi:</strong> ${newData.address}</h3>
+        `;
+
+  savedDataContainer.appendChild(card);
+}
