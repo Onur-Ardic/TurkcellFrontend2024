aa16356a4ba97dee5f158730aa03228ce310c8be
Onur Ardıç <onurardc@outlook.com>
2024-05-06 17:59:29 +0300

    bug fixed

--- Ogrenciler/OnurArdic/Takım Çalışmaları/Ders13/main.js
+++ Ogrenciler/OnurArdic/Takım Çalışmaları/Ders13/main.js
@@ -1,19 +1,19 @@
-const name = document.getElementById("name");
-const surname = document.getElementById("surname");
-const age = document.getElementById("age");
-const address = document.getElementById("address");
-const form = document.getElementById("form");
-const inputs = document.querySelectorAll("input");
-const cardWrapper = document.getElementById("cardWrapper");
-const personArray = [];
-let count = 0;
+const name = document.getElementById('name')
+const surname = document.getElementById('surname')
+const age = document.getElementById('age')
+const address = document.getElementById('address')
+const form = document.getElementById('form')
+const inputs = document.querySelectorAll('input')
+const cardWrapper = document.getElementById('cardWrapper')
+const personArray = []
+let count = 0
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const formName = name.value;
-  const formSurname = surname.value;
-  const formAge = age.value;
-  const formAddress = address.value;
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const formName = name.value
+  const formSurname = surname.value
+  const formAge = age.value
+  const formAddress = address.value
 
   const person = {
     id: count,
@@ -21,24 +21,22 @@ form.addEventListener("submit", (e) => {
     surname: formSurname,
     age: formAge,
     address: formAddress,
-  };
+  }
 
-  personArray.push(person);
-  count++;
+  personArray.push(person)
+  count++
 
   inputs.forEach((input) => {
-    input.value = "";
-    input.style.border = "1px solid blue";
-  });
+    input.value = ''
+    input.style.border = '1px solid blue'
+  })
 
-  const deleteButton = document.createElement("button");
-  deleteButton.classList.add("delete-btn");
-  deleteButton.innerText = "Delete User";
-  deleteButton.addEventListener("click", () => {
-    const index = personArray.findIndex((item) => item.id === person.id);
-    personArray.splice(index, 1);
-    cardWrapper.removeChild(card);
-  });
+  const deleteButton = document.createElement('button')
+  deleteButton.classList.add('delete-btn')
+  deleteButton.innerText = 'Delete User'
+  deleteButton.addEventListener('click', () => {
+    cardWrapper.removeChild(card)
+  })
 
   const result = `
       <div class="card-body">
@@ -47,11 +45,11 @@ form.addEventListener("submit", (e) => {
         <p class="card-text" id="age">${person.age}</p>
         <p class="card-text" id="address">${person.address}</p>
     </div>
-  `;
+  `
 
-  const card = document.createElement("div");
-  card.classList.add("card");
-  card.innerHTML = result;
-  card.appendChild(deleteButton);
-  cardWrapper.appendChild(card);
-});
+  const card = document.createElement('div')
+  card.classList.add('card')
+  card.innerHTML = result
+  card.appendChild(deleteButton)
+  cardWrapper.appendChild(card)
+})
