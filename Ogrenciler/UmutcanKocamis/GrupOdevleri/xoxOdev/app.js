9fd04fd14ff10bdc5c76f51ed4fdb5d5caf962f5
Abdullah Akkoloğlu <abdullahakkol@gmail.com>
2024-05-07 17:41:18 +0300

    xox finished

--- Ogrenciler/UmutcanKocamis/GrupOdevleri/xoxOdev/app.js
+++ Ogrenciler/UmutcanKocamis/GrupOdevleri/xoxOdev/app.js
@@ -1,8 +1,15 @@
 const buttons = document.getElementsByClassName("btn");
-
+const resetButton = document.querySelector("#resetButton");
+const xScoreText = document.querySelector("#xScore");
+const oScoreText = document.querySelector("#oScore");
+const tieScoreText = document.querySelector("#tieScore");
+const result = document.querySelector("#result");
+const table = document.querySelector("#table");
 const players = ["X", "O"];
+let xScore = 0;
+let oScore = 0;
+let tieScore = 0;
 let currentPlayer = players[0];
-
 const winConditions = [
   [0, 1, 2],
   [3, 4, 5],
@@ -13,7 +20,6 @@ const winConditions = [
   [0, 4, 8],
   [2, 4, 6],
 ];
-
 function winning(currentPlayer) {
   for (let i = 0; i < winConditions.length; i++) {
     const [a, b, c] = winConditions[i];
@@ -22,17 +28,22 @@ function winning(currentPlayer) {
       buttons[b].textContent === currentPlayer &&
       buttons[c].textContent === currentPlayer
     ) {
+      buttons[a].style.backgroundColor = "green";
+      buttons[b].style.backgroundColor = "green";
+      buttons[c].style.backgroundColor = "green";
       return true;
     }
   }
   return false;
 }
-
-/* function tie(currentPlayer) {
-  if (!winning(currentPlayer)) {
+function tie() {
+  for (let i = 0; i < buttons.length; i++) {
+    if (buttons[i].textContent === "") {
+      return false;
+    }
   }
-} */
-
+  return true;
+}
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", () => {
     if (buttons[i].textContent !== "") {
@@ -40,9 +51,36 @@ for (let i = 0; i < buttons.length; i++) {
     }
     buttons[i].textContent = currentPlayer;
     if (winning(currentPlayer)) {
-      alert(`${currentPlayer} Kazandi`);
+      table.style.pointerEvents = "none";
+      result.textContent = `${currentPlayer} Won!`;
+      if (currentPlayer === "X") {
+        xScore++;
+      } else {
+        oScore++;
+      }
+      xScoreText.textContent = `X Score: ${xScore}`;
+      oScoreText.textContent = `O Score: ${oScore}`;
+      return;
+    }
+    if (tie()) {
+      tieScore++;
+      tieScoreText.textContent = `Tie Score: ${tieScore}`;
+      result.textContent = "Tie";
       return;
     }
     currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
+    currentPlayer === players[0]
+      ? (result.textContent = "X's Turn")
+      : (result.textContent = "O's Turn");
   });
 }
+function resetGame() {
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].textContent = "";
+    buttons[i].style.backgroundColor = "#000000";
+  }
+  currentPlayer = players[0];
+  result.textContent = "X's Turn";
+  table.style.pointerEvents = "auto";
+}
+resetButton.addEventListener("click", resetGame);
