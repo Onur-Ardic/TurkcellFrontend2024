da4e12858fa3aff7009697aafde747f32668f8f2
Abdullah Akkoloğlu <abdullahakkol@gmail.com>
2024-05-08 02:30:15 +0300

    style added

--- Ogrenciler/UmutcanKocamis/GrupOdevleri/InputJsOdev/app.js
+++ Ogrenciler/UmutcanKocamis/GrupOdevleri/InputJsOdev/app.js
@@ -24,9 +24,14 @@ function addMovie(e) {
   if (checkName(moviesArray, newMovie)) {
     return;
   }
+  if (!validation()) {
+    alert("Tüm alanları doldurunuz!");
+    return;
+  }
   moviesArray.push(newMovie);
   localStorage.setItem("moviesArray", JSON.stringify(moviesArray));
   display(moviesArray);
+  clean();
 }
 function checkName(array, newMovie) {
   return array.find((movie) => movie.name == newMovie.name);
@@ -52,47 +57,64 @@ function editMovie(e) {
     genre: genreInput.value,
     image: imageInput.value,
   };
-
+  if (!validation()) {
+    alert("Tüm alanları doldurunuz!");
+    return;
+  }
   moviesArray[movieIndex] = updatedObject;
   localStorage.setItem("moviesArray", JSON.stringify(moviesArray));
   display(moviesArray);
   updateMode = false;
   submitButton.textContent = "Add";
+  clean();
 }
 
 function display(array) {
   cards.innerHTML = "";
   array.map((movie) => {
     const card = document.createElement("div");
-    card.classList.add("card", "col-3");
+    card.classList.add("card", "col-lg-6");
+    const row = document.createElement("div");
+    row.classList.add("row", "my-auto");
+    card.appendChild(row);
+
+    const imageDiv = document.createElement("div");
+    imageDiv.classList.add("col-6");
+    card.appendChild(imageDiv);
+    const imageName = document.createElement("img");
+    imageName.classList.add("img-fluid");
+    imageName.src = movie.image;
+    imageDiv.appendChild(imageName);
+
     const cardBody = document.createElement("div");
-    cardBody.classList.add("card-body");
+    cardBody.classList.add("card-body", "col-6", "p-0", "my-auto");
     card.appendChild(cardBody);
-    const title = document.createElement("h5");
-    title.classList.add("card-title");
+    const title = document.createElement("h2");
+    title.classList.add("card-title", "mb-1");
     cardBody.appendChild(title);
     title.textContent = movie.name;
 
-    const directorName = document.createElement("p");
-    directorName.classList.add("card-text");
+    row.appendChild(imageDiv);
+    row.appendChild(cardBody);
+
+    const directorName = document.createElement("h5");
+    directorName.classList.add("card-text", "mb-2", "text-secondary");
     cardBody.appendChild(directorName);
     directorName.textContent = movie.director;
 
-    const yearName = document.createElement("p");
-    yearName.classList.add("card-text");
-    cardBody.appendChild(yearName);
-    yearName.textContent = movie.year;
-
-    const genreName = document.createElement("p");
-    genreName.classList.add("card-text");
+    const genreName = document.createElement("h6");
+    genreName.classList.add("card-text", "text-success");
     cardBody.appendChild(genreName);
     genreName.textContent = movie.genre;
 
-    const imageName = document.createElement("p");
-    imageName.classList.add("card-text");
-    cardBody.appendChild(imageName);
-    imageName.textContent = movie.image;
+    const yearName = document.createElement("h6");
+    yearName.classList.add("card-text", "text-secondary");
+    cardBody.appendChild(yearName);
+    yearName.textContent = movie.year;
 
+    const buttonsDiv = document.createElement("div");
+    buttonsDiv.classList.add("d-flex", "mt-2", "gap-2");
+    cardBody.appendChild(buttonsDiv);
     const deleteButton = document.createElement("button");
     deleteButton.classList.add("btn", "btn-primary");
     deleteButton.textContent = "Delete";
@@ -108,6 +130,8 @@ function display(array) {
     card.appendChild(deleteButton);
     card.appendChild(updateButton);
     cards.appendChild(card);
+    buttonsDiv.appendChild(updateButton);
+    buttonsDiv.appendChild(deleteButton);
   });
 }
 submitButton.addEventListener("click", (e) => {
@@ -122,4 +146,23 @@ function deleteMovie(name) {
   moviesArray = moviesArray.filter((movie) => movie.name !== name);
   localStorage.setItem("moviesArray", JSON.stringify(moviesArray));
   display(moviesArray);
+}
+function validation() {
+  if (
+    nameInput.value === "" ||
+    directorInput.value === "" ||
+    yearInput.value === "" ||
+    genreInput.value === "" ||
+    imageInput.value === ""
+  ) {
+    return false;
+  }
+  return true;
+}
+function clean() {
+  nameInput.value = "";
+  directorInput.value = "";
+  yearInput.value = "";
+  genreInput.value = "";
+  imageInput.value = "";
 }
