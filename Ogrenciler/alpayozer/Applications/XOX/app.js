4c7f95683a0bc16e2d099cef652c0e8fcc296a62
Alpay Özer <alpayozer7@gmail.com>
2024-05-07 22:54:01 +0300

    xox game finished

--- Ogrenciler/alpayozer/Applications/XOX/app.js
+++ Ogrenciler/alpayozer/Applications/XOX/app.js
@@ -1,5 +1,6 @@
-let box = document.querySelectorAll(".box");
-console.log(box);
+const box = document.querySelectorAll(".box");
+const result = document.getElementById("result");
+const reset = document.getElementById("reset");
 let player = "X";
 const winningConditions = [
   [0, 1, 2],
@@ -11,14 +12,20 @@ const winningConditions = [
   [0, 4, 8],
   [2, 4, 6],
 ];
-
+let win;
 const gameBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+reset.addEventListener("click", () => {
+  location.reload();
+});
 
 const boxCheck = () => {
   for (let i = 0; i < winningConditions.length; i++) {
     const [a, b, c] = winningConditions[i];
     if (gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
-      return console.log("win");
+      result.innerText = `${player} player win !!`;
+      win = winningConditions[i];
+      return true;
     }
   }
   return null;
@@ -28,14 +35,29 @@ const boxClick = (e) => {
   let index = e.target.id;
   if (gameBoard[index] !== "X" && gameBoard[index] !== "O") {
     gameBoard[index] = player;
-    e.target.innerHTML = player;
+    e.target.innerText = player;
+    result.innerText = `${player} Turn`;
+    if (boxCheck()) {
+      box.forEach((item) => {
+        item.setAttribute("disabled", true);
+        for (let i = 0; i < win.length; i++) {
+          box[win[i]].classList.add(
+            "bg-win",
+            "animate__animated",
+            "animate__shakeX"
+          );
+        }
+      });
+      return;
+    }
     if (player === "X") {
       player = "O";
+      result.innerText = `${player} Turn`;
     } else {
       player = "X";
+      result.innerText = `${player} Turn`;
     }
   }
-  boxCheck();
 };
 
 box.forEach((item) => {
