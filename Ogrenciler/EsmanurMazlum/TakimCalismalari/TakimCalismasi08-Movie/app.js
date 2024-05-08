142abf51437d4119cf06204bef41e0b1906dab29
Erkan TOKMAK <erkan.tokmak.ofc@gmail.com>
2024-05-08 17:36:18 +0300

    localstorage bugs fixed

--- Ogrenciler/EsmanurMazlum/TakimCalismalari/TakimCalismasi08-Movie/app.js
+++ Ogrenciler/EsmanurMazlum/TakimCalismalari/TakimCalismasi08-Movie/app.js
@@ -2,11 +2,12 @@ const filmAdi = document.getElementById("filmAdi");
 const filmYonetmen = document.getElementById("filmYonetmen");
 const filmYili = document.getElementById("filmYili");
 const filmTuru = document.getElementById("filmTuru");
+
 const filmAfis = document.getElementById("filmAfis");
 
-let infos = [];
+let infos = JSON.parse(localStorage.getItem("filmler")) || [];
 
-
+let id = infos.length;
 function save() {
   let filmAdiVal = filmAdi.value;
   let filmYonetmenVal = filmYonetmen.value;
@@ -14,29 +15,30 @@ function save() {
   let filmTuruVal = filmTuru.value;
   let filmAfisVal = filmAfis.value;
 
-  let filmObject =  {
+ 
+  console.log(filmAdiVal, filmYonetmenVal, filmYiliVal, filmTuruVal)
+  infos.push({
+    filmId: id,
     filmAdi: filmAdiVal,
     filmYonetmen: filmYonetmenVal,
     filmYili: filmYiliVal,
     filmTuru: filmTuruVal,
     filmAfis: filmAfisVal,
-  };
-
-  infos = [...infos, filmObject];
-
+  })
   localStorage.setItem("filmler", JSON.stringify(infos));
 }
 
+const cardRow = document.getElementById("cardRow");
 function getMovies() {
   let filmler = JSON.parse(localStorage.getItem("filmler"));
 
-  filmler.forEach((film) => {
-    const cardRow = document.getElementById("cardRow");
+  filmler?.forEach((film) => {
     const col = document.createElement("div");
     col.classList.add("col-4");
+    col.id = `${film.filmId}`;
     cardRow.appendChild(col);
     const card = document.createElement("div");
-    card.classList.add("card");
+    card.className = "card my-3";
     col.appendChild(card);
     const cardHeader = document.createElement("div");
     cardHeader.classList.add("card-header");
@@ -44,31 +46,49 @@ function getMovies() {
     const cardBody = document.createElement("div");
     cardBody.classList.add("card-body");
     card.appendChild(cardBody);
-    const btnEdit = document.createElement("button");
-    btnEdit.className = "btn btn-secondary";
-    btnEdit.textContent = "Düzenle";
-    cardBody.appendChild(btnEdit);
-    const btnDel = document.createElement("button");
-    btnDel.className = "btn btn-danger";
-    btnDel.textContent = "Sil";
-    cardBody.appendChild(btnDel);
 
+    const cardFooter = document.createElement("div");
+    cardFooter.classList.add("card-footer");
+    card.appendChild(cardFooter);
+  
+    // console.log(film.filmId, film.filmAdi, film.filmYonetmen, film.filmYili, film.filmAfis)
+    
     const filmAdiText = document.createTextNode(film.filmAdi);
     cardHeader.appendChild(filmAdiText);
+    
+    const filmYonetmen = document.createTextNode(film.filmYonetmen);
+    let pYon = document.createElement("p");
+    pYon.appendChild(filmYonetmen);
+    cardBody.appendChild(pYon);
+     
+    const filmYili = document.createTextNode(film.filmYili);
+    let pYear = document.createElement("p");
+    pYear.appendChild(filmYili);
+    cardBody.appendChild(pYear);
+    
+    const filmTuru = document.createTextNode(film.filmTuru);
+    let p = document.createElement("p");
+    p.appendChild(filmTuru);
+    cardBody.appendChild(p)
 
     // cardHeader.textContent = film.filmAdi;
     // cardHeader.textContent = filmTuru;
     // cardHeader.textContent = filmYonetmen;
     // cardHeader.textContent = filmYili;
-  });
-
-  console.log(filmler);
-}
+    const btnEdit = document.createElement("button");
+    btnEdit.className = "btn btn-secondary w-100 mb-2";
+    btnEdit.textContent = "Düzenle";
+    cardFooter.appendChild(btnEdit);
+    const btnDel = document.createElement("button");
+    btnDel.className = "btn btn-danger w-100";
+    btnDel.textContent = "Sil";
+    cardFooter.appendChild(btnDel);
+  
+  })
+};
 
 getMovies();
 
 document.getElementById("form").addEventListener("submit", (e) => {
-  e.preventDefault();
   save();
-
 });
