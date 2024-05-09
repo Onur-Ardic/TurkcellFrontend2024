197b709f3aa4a42a5bfa7c779bf712071d9f9e76
Onur Ardıç <onurardc@outlook.com>
2024-05-09 16:15:33 +0300

    sinema rezervasyon

--- Ogrenciler/OnurArdic/MentorCalismalari/Odev02/main.js
+++ Ogrenciler/OnurArdic/MentorCalismalari/Odev02/main.js
@@ -1,3 +1,8 @@
+const firstName = document.getElementById('name')
+const surName = document.getElementById('surname')
+const ticketWrapper = document.querySelector('.ticket-card-reserved')
+const reservedValue = []
+
 window.onload = function () {
   for (let i = 0; i < localStorage.length; i++) {
     const chosenChair = localStorage.key(i)
@@ -7,7 +12,7 @@ window.onload = function () {
       if (chairElement.innerText === chosenChair) {
         chairElement.style.backgroundColor = 'green'
         chairElement.style.color = 'white'
-        chairElement.innerText = `${chosenChair} Numaralı Koltuk Rezerve Edildi. ${chosenChair}`
+        chairElement.innerText = `${chosenChair} Numaralı Koltuk Rezerve Edildi.`
       }
     })
   }
@@ -27,7 +32,6 @@ chair.forEach((chairİtem) => {
         chairİtem.style.backgroundColor = 'white'
         chairİtem.style.color = 'black'
         chairİtem.innerText = `${chairNumber}`
-        console.log(chairNumber)
         localStorage.removeItem(chairNumber)
       }
     } else {
@@ -36,10 +40,32 @@ chair.forEach((chairİtem) => {
       )
 
       if (value === true) {
+        const person = {
+          name: firstName.value,
+          surname: surName.value,
+          chairNumber: chairNumber,
+        }
         chairİtem.style.backgroundColor = 'green'
         chairİtem.style.color = 'white'
         chairİtem.innerText = `${chairNumber} Numaralı Koltuk Rezerve Edildi`
-        localStorage.setItem(chairNumber, 'reserved')
+        localStorage.setItem(`${chairNumber}`, JSON.stringify(person))
+        for (let i = 0; i < localStorage.length; i++) {
+          let key = localStorage.key(i)
+          let value = localStorage.getItem(key)
+          reservedValue.push({ key: key, value: JSON.parse(value) })
+          reservedValue.forEach((item) => {
+            console.log(item)
+            const ticket = `
+            <div class="person-info d-flex top-0 p-2 justify-content-between gap-3 position-absolute end-0">
+            <p class="name" id="name">${item.value.name}</p>
+            <p class="surname" id="surname">${item.value.surname}</p>
+            <span>${item.key}</span>
+          </div>
+            `
+
+            ticketWrapper.innerHTML += ticket
+          })
+        }
       }
     }
   })
