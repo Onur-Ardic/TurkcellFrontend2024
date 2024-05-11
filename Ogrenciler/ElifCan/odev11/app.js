d054f833e81cdfe222342bb4e1221234fb8572f7
Elif Can <76632280+ElifCan7@users.noreply.github.com>
2024-05-11 22:50:16 +0300

    project is done

--- Ogrenciler/ElifCan/odev11/app.js
+++ Ogrenciler/ElifCan/odev11/app.js
@@ -3,11 +3,12 @@ const movieDirector = document.getElementById("moviedirector");
 const movieYear = document.getElementById("year");
 const movieCategory = document.getElementById("category");
 const movieİmage = document.getElementById("movieimage");
-const addMovieForm = document.getElementById("modal-form");
+const saveMovieButton = document.getElementById("modal-form"); //inside the modal
 const addMoviebutton = document.getElementById("addMovieButton");
 const movieWrapper = document.querySelector(".movie-card-wrapper");
 const deleteBtn = document.getElementById("delete-btn");
 
+let movieNameToUpdate;
 let movieNameList = JSON.parse(localStorage.getItem("movieNameList")) || [];
 let count = 1;
 
@@ -16,12 +17,16 @@ window.onload = function () {
     addİtem(movieName.name);
   });
 };
-addMoviebutton.addEventListener("click", (e) => {
-  document.getElementById("staticBackdropLabel").innerText = "Film Ekle";
-});
-addMovieForm.addEventListener("submit", (e) => {
-  e.preventDefault();
 
+//film ekleme butonu
+addMoviebutton.addEventListener("click", () => {
+  document.getElementById("staticBackdropLabel").innerText = "Film Ekle";
+  const inputElements = staticBackdropLabelElement.querySelectorAll("input");
+  inputElement.value = "";
+});
+
+saveMovieButton.addEventListener("submit", (e) => {
+  e.preventDefault();
   const movie = {
     id: count++,
     name: movieName.value.trim(),
@@ -31,17 +36,49 @@ addMovieForm.addEventListener("submit", (e) => {
     image: movieİmage.value.trim(),
   };
 
-  movieNameList.push(movie);
+  if (document.getElementById("staticBackdropLabel").innerText == "Film Ekle") {
+    movieNameList.push(movie);
+  } else {
+    movieNameList.forEach((movieItem) => {
+      if (movieItem.name == movieNameToUpdate) {
+        movieItem.name = movie.name;
+        movieItem.director = movie.director;
+        movieItem.year = movie.year;
+        movieItem.category = movie.category;
+        movieItem.image = movie.image;
+
+        let movieCard = document.getElementById(movieNameToUpdate);
+
+        let mName = movieCard.querySelector("h5");
+        mName.innerText = movieItem.name;
+
+        let mYear = movieCard.querySelector("p");
+        mYear.innerText = movieItem.year;
+
+        let director = movieCard.querySelector("p:nth-child(2)");
+        director.innerText = movieItem.director;
+
+        let mcategory = movieCard.querySelector("span");
+        mcategory.innerText = movieItem.category;
+
+        let mImg = movieCard.querySelector("img").src;
+        mImg = movieItem.image;
+      }
+    });
+  }
+
   localStorage.setItem("movieNameList", JSON.stringify(movieNameList));
-  addİtem(movie.name);
+  console.log(movie.name);
+  if (document.getElementById("staticBackdropLabel").innerText == "Film Ekle") {
+    addİtem(movie.name);
+  }
 });
 
 function addİtem(movieValue) {
-  const movieNameList = JSON.parse(localStorage.getItem("movieNameList")) || [];
   const movieİtem = movieNameList.find((movie) => movie.name === movieValue);
 
   const value = `
-      <div class="card position-relative col-4" data-key="${movieİtem.name}">
+      <div id=${movieİtem.name} class="card position-relative col-4" data-key="${movieİtem.name}">
       <div class="card-image w-100">
         <img
           src="${movieİtem.image}"
@@ -49,8 +86,8 @@ function addİtem(movieValue) {
           class="img-fluid rounded-top object-fit-cover  "
         />
       </div>
-      <div class="card-content p-1 text-white">
-        <h5 class="text-center" contenteditable="true">${movieİtem.name}</h5>
+      <div  class="card-content p-1 text-white">
+        <h5 class="text-center"  contenteditable="true">${movieİtem.name}</h5>
         <div class="movie-card-info d-flex justify-content-center gap-3 align-item-center">
           <p contenteditable="true">${movieİtem.year}</p>
           <p contenteditable="true">${movieİtem.director}</p>
@@ -91,24 +128,14 @@ function deleteMovie(movieValue) {
 }
 
 function updateMovie(movieValue) {
-  //movie bilgilerini aldım.
   document.getElementById("staticBackdropLabel").innerText = "Güncelle";
   const movie = movieNameList.find((movie) => movie.name === movieValue);
+  movieNameToUpdate = movie.name;
   document.getElementById("moviename").value = movie.name;
   document.getElementById("moviedirector").value = movie.director;
   document.getElementById("year").value = movie.year;
   document.getElementById("category").value = movie.category;
   document.getElementById("movieimage").value = movie.image;
-  if (document.getElementById("staticBackdropLabel").innerText == "Güncelle") {
-    elif(movieValue);
-  }
-}
-function elif(movieValue) {
-  console.log("eliff");
-  movieNameList = movieNameList.filter((movie) => movie.name !== movieValue);
-  movieNameList.push(movie);
-  localStorage.setItem("movieNameList", JSON.stringify(movieNameList));
-  // deleteMovie(movieValue);
 }
 
 const selectElementFilter = document.getElementById("categoryFilter");
@@ -125,15 +152,14 @@ const movieCategories = [
   "Horror",
   "Animation",
 ];
-const optionAllCategories = document.createElement("option");
-optionAllCategories.textContent = "All Categories";
-optionAllCategories.textContent = "All Categories";
-selectElementFilter.appendChild(optionAllCategories);
+// const optionAllCategories = document.createElement("option");
+// optionAllCategories.textContent = "All Categories";
+// selectElementFilter.appendChild(optionAllCategories);
 
 movieCategories.forEach((category) => {
   const option = document.createElement("option");
   option.textContent = category;
-  option.textContent = category;
+  //   option.textContent = category;
 
   selectElementFilter.appendChild(option);
 
@@ -218,7 +244,6 @@ document
 
     //inputtaki value değerini alık.
     const inputValue = this.value;
-    console.log(inputValue); // Hata burada
     const filteredMovies = movieNameList.filter((movie) => {
       return movie.category.includes(inputValue);
     });
---
+++ Ogrenciler/ElifCan/odev11/movie.js
@@ -0,0 +1,7 @@
+function movie(name, director, year, category, image) {
+  this.name = name;
+  this.director = director;
+  this.year = year;
+  this.category = category;
+  this.image = image;
+}
