24473475f634cc9fe2eaeebefbd7bca0e91c0706
Abdullah Akkoloğlu <abdullahakkol@gmail.com>
2024-05-12 18:20:37 +0300

    prototype hw finished

--- Ogrenciler/NecatiDogrul/Odevler/Prototype/app.js
+++ Ogrenciler/NecatiDogrul/Odevler/Prototype/app.js
@@ -1,26 +1,37 @@
-import {Movie} from "./movie.js";
-import { Storage } from "./storage.js";
-
 const nameInput = document.querySelector("#name");
 const directorInput = document.querySelector("#director");
 const yearInput = document.querySelector("#year");
 const genreInput = document.querySelector("#genre");
 const imageInput = document.querySelector("#image");
 const submitButton = document.querySelector("#submitButton");
-const movieForm = document.querySelector("#movieForm");
 const cards = document.querySelector(".cards");
 let updateMode = false;
 let movieIndex;
-
-
-submitButton.addEventListener("click",(e)=>{
-    e.preventDefault();
-    const movie= new Movie(nameInput.value,directorInput.value,yearInput.value,genreInput.value,imageInput.value);
-    console.log(movie);
-})
-
 const storage = new Storage();
-console.log(storage);
-
-
-
+const ui = new UI();
+ui.display(storage.getMoviesLocal());
+submitButton.addEventListener("click", (e) => {
+  updateMode ? editMovie(e) : addMovie(e);
+});
+function addMovie(e) {
+  e.preventDefault();
+  const movie = new Movie(
+    nameInput.value,
+    directorInput.value,
+    yearInput.value,
+    genreInput.value,
+    imageInput.value
+  );
+  storage.setMoviesLocal(movie);
+}
+function editMovie(e) {
+  e.preventDefault();
+  const updatedObject = {
+    name: nameInput.value,
+    director: directorInput.value,
+    year: yearInput.value,
+    genre: genreInput.value,
+    image: imageInput.value,
+  };
+  storage.editMovie(updatedObject);
+}
