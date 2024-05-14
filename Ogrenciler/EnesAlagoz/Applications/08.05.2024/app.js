d323b5202e43ec3250d351a3c645a240ebda61c5
enesalagoz1212 <118561378+enesalagoz1212@users.noreply.github.com>
2024-05-14 20:32:21 +0300

    create

---
+++ Ogrenciler/EnesAlagoz/Applications/08.05.2024/app.js
@@ -0,0 +1,34 @@
+// Değişkenler
+// var name; // sayısal ifede ile başlayamaz
+// Undefined
+// var for; komut isimleri ile tanımlama yapılamaz
+
+// var nameSurname;
+
+// var let const
+
+// var x;
+// x = "Enes";
+// var y;
+// y = 20;
+// console.log(typeof x);
+// console.log(typeof y);
+
+// let fullName = "Enes Alagöz";
+// console.log(fullName);
+
+// const email = "enes14@gmail.com";
+// email = "enes@gmail.com";    // const işleminde değişim yapmazsın
+// console.log(email);
+
+// alert("Enes");
+// console.log("Enes");
+
+var name = "Enes";
+console.log(typeof name);
+
+function MyCLass() {
+  let name = "Enes";
+  console.log(name);
+}
+MyCLass();
