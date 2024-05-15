2e09b068960d50de4b42e28a4002edd7283a176e
Fatih <fdonmez_1512@hotmail.com>
2024-05-15 12:30:56 +0300

    Add OOP to the project

--- Ogrenciler/Egemen/Grup-Odevleri/Ders21-Sinema-Koleksiyon/script.js
+++ Ogrenciler/Egemen/Grup-Odevleri/Ders21-Sinema-Koleksiyon/script.js
@@ -95,7 +95,6 @@ function saveFilmsToStorage(films) {
 }
 function loadFilms() {
   const films = getFilmsFromStorage();
-  // console.log(films);
   films.forEach((film, index) => {
     console.log(film);
     film = new Movie(film);
@@ -104,34 +103,3 @@ function loadFilms() {
     film.displayCard(index);
   });
 }
-
-function Movie({ name, director, year, genre, posterUrl }) {
-  this.name = name;
-  this.director = director;
-  this.year = year;
-  this.genre = genre;
-  this.posterUrl = posterUrl;
-}
-Movie.prototype.egemen = function ({ filmname }) {
-  console.log("test1");
-};
-Movie.prototype.displayCard = function (index) {
-  const filmCard = document.createElement("div");
-  filmCard.className = "film-card";
-  filmCard.innerHTML = `
-          <div class="film-details">
-              <img src="${this.posterUrl}" alt="${this.name}">
-              <div class="film-info">
-                  <strong>${this.name}</strong>
-                  <p>Yönetmen: ${this.director}</p>
-                  <p>Yıl: ${this.year}</p>
-                  <p>Tür: ${this.genre}</p>
-              </div>
-              <div class="card-buttons">
-                  <button onclick="editFilm(${index})">Güncelle</button>
-                  <button onclick="deleteFilm(${index})">Sil</button>
-              </div>
-          </div>
-      `;
-  filmCollection.appendChild(filmCard);
-};
