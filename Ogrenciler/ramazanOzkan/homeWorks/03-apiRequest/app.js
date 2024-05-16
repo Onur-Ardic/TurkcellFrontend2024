113ccde3f6c791f7ba6c4b38ec12883e6e70b267
ramazanozkan777 <54441645+ramazanozkan777@users.noreply.github.com>
2024-05-17 00:12:59 +0300

    get request
    v2.0

--- Ogrenciler/ramazanOzkan/homeWorks/03-apiRequest/app.js
+++ Ogrenciler/ramazanOzkan/homeWorks/03-apiRequest/app.js
@@ -1,49 +1,76 @@
-document.getElementById("getButton").addEventListener("click", getAllData);
-function getAllData() {
-  const xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://jsonplaceholder.typicode.com/posts", true);
-  xhr.onload = function () {
-    let cards = document.getElementById("cards");
-    if (this.status === 200) {
-      const data = JSON.parse(this.responseText);
-      const limitedData = data.slice(0, 20);
-      //https://picsum.photos/300/185
-      limitedData.forEach((user) => {
-       // xhr.open("GET", "https://picsum.photos/300/185", true);
-        const randomImage = `https://picsum.photos/id/${Math.round((Math.random()*5))+10}/300/185`
-        const card = document.createElement("div");
-        const limitedTitle = user.body.length > 50 ? user.body.substring(0, 50) + '...' : user.body; 
-        cards.innerHTML += `
-        <div class="card" style="width: 18rem;">
-          <img src=${randomImage} class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">${user.title}</h5>
-              <p class="card-text">${limitedTitle}</p>
-              <a href="#" class="btn btn-primary" id="oku">Devamını Oku</a>
-            </div>
-        </div>       
-        `;
+class CardManager {
+  constructor(apiUrl, containerId) {
+      this.apiUrl = apiUrl;
+      this.container = document.getElementById(containerId);
+  }
+
+  getAlldata() {
+      const xhr = new XMLHttpRequest();
+      xhr.open("GET", this.apiUrl, true);
+      xhr.onload = () => {
+          if (xhr.status === 200) {
+              const data = JSON.parse(xhr.responseText);
+              const limitedData = data.slice(0, 20);
+              this.renderCards(limitedData);
+          } else {
+              console.error("Bir hata oluştu, veriler alınamadı.");
+          }
+      };
+      xhr.send();
+  }
+
+  renderCards(data) {
+      data.forEach(user => {
+          const card = this.createCard(user);
+          this.container.appendChild(card);
       });
+  }
+
+  createCard(user) {
+      const card = document.createElement('div');
+      card.className = 'card';
       
-    } 
-    else {
-      console.log("Bir hata oluştu, veriler alınamadı.");
-    }
-    // const okuButton = document.getElementById("oku");
-    // okuButton.addEventListener("click",()=>{
-    //   window.open("https://jsonplaceholder.typicode.com/posts/"+user.id)
-    // });
+      const randomImage = `https://picsum.photos/id/${Math.round((Math.random() * 10)) + 10}/300/185`;
+      const limitedTitle = user.body.length > 50 ? user.body.substring(0, 50) + '...' : user.body;
 
+      const img = document.createElement('img');
+      img.src = randomImage;
+      img.className = 'card-img-top';
+      img.alt = 'ÖrnekResim';
+      card.appendChild(img);
 
+      const cardBody = document.createElement('div');
+      cardBody.className = 'card-body';
+      card.appendChild(cardBody);
 
-  };
-  xhr.send();
+      const title = document.createElement('h5');
+      title.className = 'card-title';
+      title.textContent = user.title;
+      cardBody.appendChild(title);
+
+      const text = document.createElement('p');
+      text.className = 'card-text';
+      text.textContent = limitedTitle;
+      cardBody.appendChild(text);
+
+      const button = document.createElement('button');
+      button.className = 'btn btn-primary';
+      button.id = 'oku';
+      button.textContent = 'Devamını Oku';
+      button.onclick = () => {
+          this.openBlog(user.id);
+      };
+      cardBody.appendChild(button);
+
+      return card;
+  }
+
+  openBlog(id) {
+      window.open(`blog.html?id=${id}`, "_blank");
+  }
 }
 
-
-// list.innerHTML += `
-// <tr>
-//   <td>${user.title}</td>
-//   <td>${user.body}</td>
-// </tr>
-// 
+window.onload = function() {
+  const cardManager = new CardManager("https://jsonplaceholder.typicode.com/posts", "cards");
+  cardManager.getAlldata();
+};
