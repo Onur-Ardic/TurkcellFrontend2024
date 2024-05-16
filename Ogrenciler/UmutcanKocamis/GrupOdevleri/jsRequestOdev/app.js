8fd808fdba382818deb6d16c0792cf0204f3f48b
Umutcan Kocamis <umutceaan@gmail.com>
2024-05-16 23:27:40 +0300

    blogger homework finished

--- Ogrenciler/UmutcanKocamis/GrupOdevleri/jsRequestOdev/app.js
+++ Ogrenciler/UmutcanKocamis/GrupOdevleri/jsRequestOdev/app.js
@@ -1,19 +1,15 @@
 document.getElementById("getButton").addEventListener("click", getAllData);
-const post = new Post();
 function getAllData() {
   const xhr = new XMLHttpRequest();
   xhr.open("GET", "https://jsonplaceholder.typicode.com/posts", true);
   xhr.onload = function () {
-    let list = document.querySelector(".card");
+    let list = document.querySelector("#cards");
     if (this.status === 200) {
       const data = JSON.parse(this.responseText);
       data.forEach((post) => {
+        let random = Math.round(Math.random() * 100);
         if (post.id <= 20) {
-          list.innerHTML += `
-          <img src="https://picsum.photos/id/${Post.randomNumber()}/200/300">
-          <h3>${post.title}</h3>
-          <p>${post.body}</p>
-          `;
+          list.innerHTML += Post.createPost(post.title, post.body, random);
         }
       });
     } else {
--- Ogrenciler/UmutcanKocamis/GrupOdevleri/jsRequestOdev/post.js
+++ Ogrenciler/UmutcanKocamis/GrupOdevleri/jsRequestOdev/post.js
@@ -1,10 +1,14 @@
 class Post {
-  constructor(title, body) {
-    this.title = title;
-    this.body = body;
-  }
-  static randomNumber() {
-    let random = Math.round(Math.random() * 100);
-    return random;
+  static createPost(title, body, random) {
+    return `
+    <div class="col-4 ">
+      <div class="card mb-3" style="height:45rem;">
+        <img src="https://picsum.photos/id/${random}/500/500" class="card-img-top">
+        <div class="card-body">
+          <h3 class="card-title">${title}</h3>
+          <p class="card-text">${body}</p>
+        </div>
+      </div>
+   </div>`;
   }
 }
