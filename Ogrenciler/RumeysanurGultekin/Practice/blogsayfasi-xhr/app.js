380b7be85b4ebaae61a842a4753cc73e001812d3
rumeysanurgultekin <163021500+rumeysanurgultekin@users.noreply.github.com>
2024-05-17 16:30:31 +0300

    xhr güncelleme

--- Ogrenciler/RumeysanurGultekin/Practice/blogsayfasi-xhr/app.js
+++ Ogrenciler/RumeysanurGultekin/Practice/blogsayfasi-xhr/app.js
@@ -1,3 +1,38 @@
+
+
+function createBlog (post) {
+     let deneme = document.getElementById("deneme");
+
+     let card = document.createElement("div");
+     card.classList.add("card" , "border" , "w-25") ;
+     
+    
+     let img = document.createElement("img");
+     img.classList.add("card-img-top");
+     img.src = `https://picsum.photos/id/${post.id}/200/200` ;
+     
+
+     let cardBody = document.createElement("div");
+     cardBody.classList.add("card-body");
+     
+
+     let h5 = document.createElement("h5");
+     h5.classList.add("card-title" , "text-uppercase" , "fs-5");
+     h5.innerHTML = post.title;
+     cardBody.appendChild(h5);
+
+     let p = document.createElement("p");
+     p.classList.add("card-text");
+     p.innerHTML = post.body;
+     cardBody.appendChild(p);
+
+     card.appendChild(img);
+     card.appendChild(cardBody);
+     deneme.appendChild(card);
+
+
+}
+
 window.onload = function() {
     bloguGetir();
 };
@@ -7,20 +42,22 @@ function bloguGetir(){
         xhr.open("GET", "https://jsonplaceholder.typicode.com/posts", true);
         xhr.onload = function () {  
             if (this.status === 200) {
-                const data = JSON.parse(xhr.responseText);
                 let blogyazisi = document.getElementById("deneme");
+                const data = JSON.parse(xhr.responseText);
                 for (let i = 0; i < 20 && i < data.length; i++) {
                     let post = data[i];
-                        blogyazisi.innerHTML +=`
-                        <div class="row">
-                            <div class="card border col-3" style="width: 25rem;">
-                                <img class="card-img-top" src="https://picsum.photos/id/${post.id}/200/200" alt="Card image cap">
-                                <div class="card-body">
-                                    <h5 class="card-title text-uppercase fs-5">${post.title}</h5>
-                                    <p class="card-text ">${post.body}</p>
-                                </div>
-                            </div>
-                        </div>    `;
+                        createBlog(post);
+                        
+                        // blogyazisi.innerHTML +=
+                        // `<div class="d-flex">
+                        //     <div class="card border" style="width: 25rem;">
+                        //         <img class="card-img-top" src="https://picsum.photos/id/${post.id}/200/200" alt="Card image cap">
+                        //         <div class="card-body">
+                        //             <h5 class="card-title text-uppercase fs-5">${post.title}</h5>
+                        //             <p class="card-text ">${post.body}</p>
+                        //         </div>
+                        //     </div>
+                        // </div>    `;
 
                     }
             }else{
