91c97e255e9ab7ca6ccb10f0dde27264680becde
Onur Ardıç <onurardc@outlook.com>
2024-05-20 16:27:23 +0300

    search tamamlandı

--- Ogrenciler/OnurArdic/Odevler/Odev02/filter.js
+++ Ogrenciler/OnurArdic/Odevler/Odev02/filter.js
@@ -1,4 +1,4 @@
-function filterHandler(bookCard, book) {
+function filterHandler(bookCard) {
   const categoryFilter = document.getElementById('bookCategoryFilter')
   const cardCategory = bookCard.querySelector('.book-card-category')
 
@@ -13,3 +13,19 @@ function filterHandler(bookCard, book) {
     }
   })
 }
+
+document.querySelector('.btn-info').addEventListener('click', function (event) {
+  event.preventDefault()
+  const searchInput = document.querySelector('input[type="text"]').value.toLowerCase().trim()
+  const bookCards = document.querySelectorAll('.book-card')
+
+  bookCards.forEach((bookCard) => {
+    const bookName = bookCard.querySelector('h5').textContent.toLowerCase().trim()
+    const bookAuthor = bookCard.querySelector('.book-card-author').textContent.toLowerCase().trim()
+    if (bookName.includes(searchInput) || bookAuthor.includes(searchInput)) {
+      bookCard.style.display = 'block'
+    } else {
+      bookCard.style.display = 'none'
+    }
+  })
+})
