c4e85a2aee6f61c3fc1c984859c21a64671445ac
ŞEVVAL ERHAN <sevvalerhan8@gmail.com>
2024-05-20 17:11:41 +0300

    odev

--- Ogrenciler/SevvalErhan/Dersler/Ders-18/Json-Server/app.js
+++ Ogrenciler/SevvalErhan/Dersler/Ders-18/Json-Server/app.js
@@ -1,3 +1,5 @@
+const usersDiv = document.getElementById("users");
+
 class Request {
   static get(url) {
     return new Promise((resolve, reject) => {
@@ -7,101 +9,27 @@ class Request {
         .catch((err) => reject(err, "Veri alınamadı."));
     });
   }
-  static post(url, data) {
-    return new Promise((resolve, reject) => {
-      fetch(url, {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-          "Content-type": "db/json;",
-        },
-      })
-        .then((response) => response.json())
-        .then((data) => resolve(data))
-        .catch((err) => reject(err, "Hata Alındı."));
-    });
-  }
-  static put(url, data) {
-    return new Promise((resolve, reject) => {
-      fetch(url, {
-        method: "PUT",
-        body: JSON.stringify(data),
-        headers: {
-          "Content-type": "db/json;",
-        },
-      })
-        .then((response) => response.json())
-        .then((data) => resolve(data))
-        .catch((err) => reject(err, "Hata Alındı."));
-    });
-  }
-  static delete(url) {
-    return new Promise((resolve, reject) => {
-      fetch(url, {
-        method: "DELETE",
-      })
-        .then((response) => response.json())
-        .then(() => resolve("Veri Silindi"))
-        .catch((err) => reject(err, "Hata Alındı."));
+}
+
+class UI {
+  static createCard(users) {
+    users.forEach((user) => {
+      const userDiv = document.createElement("div");
+      userDiv.classList.add("col-lg-3");
+      userDiv.innerHTML = `
+            <div class="card"">
+                <div class="card-body">
+                    <h5 class="card-title">${user.name}</h5>
+                    <p class="card-text">${user.department}</p>
+                    <p class="card-text">${user.salary}</p>
+                </div>
+            </div>
+                `;
+      usersDiv.appendChild(userDiv);
     });
   }
 }
-Request.get("http://localhost:3000/users")
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
 
-Request.post("http://localhost:3000/users", {
-  id: 11,
-  name: "Deneme Deneme",
-  username: "Bret",
-  email: "Sincere@april.biz",
-  address: {
-    street: "Kulas Light",
-    suite: "Apt. 556",
-    city: "Gwenborough",
-    zipcode: "92998-3874",
-    geo: {
-      lat: "-37.3159",
-      lng: "81.1496",
-    },
-  },
-  phone: "1-770-736-8031 x56442",
-  website: "hildegard.org",
-  company: {
-    name: "Romaguera-Crona",
-    catchPhrase: "Multi-layered client-server neural-net",
-    bs: "harness real-time e-markets",
-  },
-})
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
-
-Request.put("http://localhost:3000/users", {
-  id: 1,
-  name: "Deneme Deneme",
-  username: "Bret",
-  email: "Sincere@april.biz",
-  address: {
-    street: "Kulas Light",
-    suite: "Apt. 556",
-    city: "Gwenborough",
-    zipcode: "92998-3874",
-    geo: {
-      lat: "-37.3159",
-      lng: "81.1496",
-    },
-  },
-  phone: "1-770-736-8031 x56442",
-  website: "hildegard.org",
-  company: {
-    name: "Romaguera-Crona",
-    catchPhrase: "Multi-layered client-server neural-net",
-    bs: "harness real-time e-markets",
-  },
-})
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
-
-Request.delete("http://localhost:3000/users")
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
+Request.get("http://localhost:3000/users").then((response) => {
+  UI.createCard(response);
+});
