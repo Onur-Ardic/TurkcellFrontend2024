8c689bf7ab0d53d72f30553fdc1ad1031b13209f
Barış Erkuş <1bariserkus@gmail.com>
2024-05-22 14:26:51 +0300

    last version

--- Ogrenciler/BarisErkus/Odevler/Odev2/project/js/helpers/api-helper.js
+++ Ogrenciler/BarisErkus/Odevler/Odev2/project/js/helpers/api-helper.js
@@ -1,5 +1,5 @@
 class APIHelper {
     static isValid(book) {
-        return book.title && book.coverUrl && book.genre && book.year && book.description && book.author;
+        return book.title && book.coverUrl && book.genre && book.year && book.description && book.author.authorName && book.author.authorImageUrl && book.author.biography;
     }
 }
