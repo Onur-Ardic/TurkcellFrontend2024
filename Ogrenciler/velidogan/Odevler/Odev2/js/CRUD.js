547e9a5896bd46057967a295fbefd0055ca4b7a4
Veli Dogan <velidogan120@gmail.com>
2024-05-22 10:06:06 +0300

    Odev 2

---
+++ Ogrenciler/velidogan/Odevler/Odev2/js/CRUD.js
@@ -0,0 +1,60 @@
+class Request{
+    static async get(url,sort){
+        try{
+            const response = await fetch(url + sort);
+            if(!response.ok)
+                throw new Error("Kitap bulunamadı.")
+            const data = await response.json();
+            return data;
+        } catch (err) {
+            ui.error(err.message);
+        }
+    }
+    static async post(url,data){
+        try {
+            const response = await fetch(url,{
+                method: "POST",
+                body: JSON.stringify(data),
+                headers: {
+                "Content-type": "application/json;",
+                }
+            });
+            if(!response.ok){
+                e.preventDefault()
+                throw new Error("Kitap Eklenemedi.")
+            }
+
+        } catch (err) {
+            ui.error(err.message);
+        }
+    }
+    static async put(url,data,id){
+        try {
+            const response = await fetch(url + "/" + id, {
+                method: "PUT",
+                body: JSON.stringify(data),
+                headers: {
+                "Content-type": "application/json;",
+                }
+            });
+            if(!response.ok){
+                e.preventDefault()
+                throw new Error("Kitap Düzenlenemedi.")
+            }
+        } catch (err) {
+            ui.error(err.message);
+        }
+    }
+    static async delete(url,id){
+        try {
+            const response = await fetch(url + "/" + id,{
+                method: "DELETE"
+            }); 
+            if(!response.ok){
+                throw new Error("Kitap Silinemedi.")
+            }
+        } catch (err) {
+            ui.error(err.message);
+        }       
+    }
+}
---
+++ Ogrenciler/velidogan/Odevler/Odev2/js/app.js
@@ -0,0 +1,91 @@
+const ui = new UI();
+let editId;
+let editStatus = false;
+
+window.addEventListener("load",() => {
+    Request.get("http://localhost:3000/books"," ")
+        .then(data => ui.displayBooks(data));
+})
+
+ui.bookForm.addEventListener("submit",async (e) => {
+    if(!editStatus){
+        const book = await new Book(crypto.randomUUID(),ui.bookName.value,ui.author.value,ui.category.value,ui.date.value,ui.imgUrl.value);
+        Request.post("http://localhost:3000/books",book)
+    }else{
+        const editBook = await new Book(editId,ui.bookName.value,ui.author.value,ui.category.value,ui.date.value,ui.imgUrl.value);
+        Request.put("http://localhost:3000/books",editBook,editId);
+        ui.bookForm.querySelector("button").textContent = "Submit";
+    }
+})
+
+function deleteBook(id){
+    Request.delete("http://localhost:3000/books",id)
+}
+
+function selectedBook(book) {
+    editId = book.id;
+    editStatus = true;
+    ui.bookName.value = book.name;
+    ui.author.value = book.author;
+    ui.category.value = book.category;
+    ui.date.value = book.date;
+    ui.imgUrl.value = book.imgUrl;
+    ui.bookForm.querySelector("button").textContent = "Update";
+    ui.bookAddBtn.click();
+}
+
+ui.sort.addEventListener("change",(e) => {
+    const key = e.target.value;
+    switch (key) {
+        case "1":
+            Request.get("http://localhost:3000/books?_sort=","name")
+                .then(data => ui.displayBooks(data));
+        break;
+        case "2":
+            Request.get("http://localhost:3000/books?_sort=name","&_order=desc")
+                .then(data => ui.displayBooks(data));
+        break;
+        case "3":
+            Request.get("http://localhost:3000/books?_sort=","date")
+                .then(data => ui.displayBooks(data));
+        break;
+        default:
+            Request.get("http://localhost:3000/books","")
+                .then(data => ui.displayBooks(data));
+        break;
+    }
+})
+
+ui.searchName.addEventListener("input",(e) => {
+    let key = e.target.value;
+    Request.get("http://localhost:3000/books?name_like=",key)
+        .then(data => ui.displayBooks(data));
+})
+
+ui.searchAuthor.addEventListener("input",(e) => {
+    let key = e.target.value;
+    Request.get("http://localhost:3000/books?author_like=",key)
+        .then(data => ui.displayBooks(data));
+})
+
+ui.searchCategory.addEventListener("change",(e) => {
+    const key = e.target.value;
+    switch (key) {
+        case "Novel":
+            Request.get("http://localhost:3000/books?category=","Novel")
+                .then(data => ui.displayBooks(data));
+        break;
+        case "Historical":
+            Request.get("http://localhost:3000/books?category=","Historical")
+                .then(data => ui.displayBooks(data));
+        break;
+        case "Religious":
+            Request.get("http://localhost:3000/books?category=","Religious")
+                .then(data => ui.displayBooks(data));
+        break;
+        default:
+            Request.get("http://localhost:3000/books","")
+                .then(data => ui.displayBooks(data));
+        break;
+    }
+})
