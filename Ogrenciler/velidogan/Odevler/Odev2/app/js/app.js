a40d39db91299eef1eeef9ca768ec3961ebd9c90
Veli Dogan <velidogan120@gmail.com>
2024-05-22 10:45:50 +0300

    Odev2 edited

--- Ogrenciler/velidogan/Odevler/Odev2/app/js/app.js
+++ Ogrenciler/velidogan/Odevler/Odev2/app/js/app.js
@@ -13,6 +13,7 @@ ui.bookForm.addEventListener("submit",async (e) => {
         Request.post("http://localhost:3000/books",book);
     }else{
         const editBook = await new Book(editId,ui.bookName.value,ui.author.value,ui.category.value,ui.date.value,ui.imgUrl.value);
+        console.log(editBook)
         Request.put("http://localhost:3000/books",editBook,editId);
         ui.bookForm.querySelector("button").textContent = "Submit";
     }
