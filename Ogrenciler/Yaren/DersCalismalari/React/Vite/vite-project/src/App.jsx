cfaf802c9d97fcc232c10930d1ef14a9fbf2a975
Yaren Su <atasu07@hotmail.com>
2024-05-24 22:43:12 +0300

    React giris

---
+++ Ogrenciler/Yaren/DersCalismalari/React/Vite/vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import ".App.css";
+import Deneme from "./components/Deneme";
+
+function App() {
+  return (
+    <>
+      <Deneme isim="Yaren" />
+      {/* 
+            <DenemeClass isim="Yaren" />  */}
+    </>
+  );
+}
+export default App;
