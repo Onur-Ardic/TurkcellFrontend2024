4749d1efcca128d13ef19d04b1b1aa1b18ddbe6e
Barış Erkuş <1bariserkus@gmail.com>
2024-05-24 20:56:55 +0300

    lesson 18

---
+++ Ogrenciler/BarisErkus/Lessons/lesson-18-react/vite/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Deneme from './components/Deneme'
+// import DenemeClass from './components/DenemeClass'
+
+function App() {
+  return (
+    <>
+      <Deneme isim="Denemeeeeeeeeeeeeeeee" />
+      {/* <Deneme isim="Denem 2" yas="25" /> */}
+      {/* <DenemeClass isim="Barış" /> */}
+    </>
+  )
+}
+
+export default App
