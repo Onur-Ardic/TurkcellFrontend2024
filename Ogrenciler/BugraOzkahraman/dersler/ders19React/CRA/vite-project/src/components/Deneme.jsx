0fafba0ec4aaf73b525f4e9f8358211d6bbfc3cf
Bugra034 <Bugraozk58@gmail.com>
2024-05-24 20:57:04 +0300

    react giris

---
+++ Ogrenciler/BugraOzkahraman/dersler/ders19React/CRA/vite-project/src/components/Deneme.jsx
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types";
+import { useState } from "react";
+
+const Deneme = ({
+  isim = "isim bilgisi girilemdi",
+  yas = "yaş biligisi girilmedi",
+}) => {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <div>{isim}</div>
+      <div>{yas}</div>
+      <div>{count}</div>
+      <button onClick={() => setCount(count + 1)}>Artır</button>
+    </>
+  );
+};
+
+Deneme.propTypes = {
+  isim: PropTypes.string.isRequired,
+  yas: PropTypes.string,
+};
+
+export default Deneme;
