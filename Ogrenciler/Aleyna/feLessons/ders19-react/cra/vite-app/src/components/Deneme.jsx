7e6ff52e74f3d77e0a685e895386d8148af90217
aleynaertok <ertok74@gmail.com>
2024-05-24 20:55:11 +0300

    react added

---
+++ Ogrenciler/Aleyna/feLessons/ders19-react/cra/vite-app/src/components/Deneme.jsx
@@ -0,0 +1,20 @@
+import PropTypes from "prop-types";
+import { useState } from "react";
+const Deneme = ({ isim = "İsim bilgisi yok", yas = "Yaş bilgisi yok" }) => {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <div>{isim}</div>
+      <div>{yas}</div>
+      <div>{count}</div>
+      <button onClick={() => setCount(count + 1)}>Artar</button>
+    </>
+  );
+};
+
+Deneme.propTypes = {
+  isim: PropTypes.string.isRequired,
+  yas: PropTypes.string,
+};
+
+export default Deneme;
