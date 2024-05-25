1192135ad504746a9591c31fe7c2b053fd42593a
enesalagoz1212 <118561378+enesalagoz1212@users.noreply.github.com>
2024-05-25 07:53:53 +0300

    create

---
+++ Ogrenciler/EnesAlagoz/Lessons/Ders19/vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Deneme from "./components/Deneme";
+
+function App() {
+  return (
+    <>
+      <Deneme isim="Varol" yas="23" />
+      <Deneme isim="Ahmet" />
+      <Deneme isim="Mehmet" yas="20" />
+    </>
+  );
+}
+
+export default App;
