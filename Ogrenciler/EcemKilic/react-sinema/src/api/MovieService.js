1c66aff21963b6b52513d98846ad0cb05c1821a0
Ecem Kılıç <kilic0ecem@gmail.com>
2024-05-27 16:59:05 +0300

    react movie collection

---
+++ Ogrenciler/EcemKilic/react-sinema/src/api/MovieService.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+export async function addFilmToDb(film) {
+  try {
+    const response = await axios.post("http://localhost:3000/movies", film);
+    return response.data;
+  } catch (error) {
+    console.error("Error adding film to the database:", error);
+    throw error;
+  }
+}
+
+export async function getFilmsFromDb() {
+  try {
+    const response = await axios.get("http://localhost:3000/movies");
+    return response.data;
+  } catch (error) {
+    console.error("Error retrieving films from the database:", error);
+    throw error;
+  }
+}
+
+export async function deleteFilmFromDb(id) {
+  try {
+    const response = await axios.delete(`http://localhost:3000/movies/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error deleting films from the database:", error);
+    throw error;
+  }
+}
