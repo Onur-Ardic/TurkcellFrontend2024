3eb5799c782be743921d178b076ccd864f900aac
karakayaezgi <karakayaezgi620@gmail.com>
2024-05-28 19:52:22 +0300

    to-do-app update

---
+++ Ogrenciler/Ezgi/Grup-Calismalari/React-to-do-list/src/components/TodoList.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Todo from './Todo'
+
+const TodoList = ({ todos, setTodos }) => {
+    return (
+        <>
+            <div className="container">
+                <ul className='todo-list'>
+
+                    {todos.map((todo) => (
+                        <Todo
+                            text={todo.text}
+                            todo={todo}
+                            key={todo.id}
+                            todos={todos}
+                            setTodos={setTodos}
+                        />
+                    ))}
+
+                </ul>
+            </div>
+        </>
+    )
+}
+
+export default TodoList
--- Ogrenciler/Ezgi/personal-work/confirmEnding/app.js
+++ Ogrenciler/Ezgi/personal-work/confirmEnding/app.js
@@ -2,11 +2,13 @@ const confirmEnding = (str, target) => {
     
     let endOfString = str.slice(-target.length); //sağdan başlarken -1 ile başlıyor.
     
-    if (endOfString === target) {
-        return true;
-    } else {
-        return false;
-    }
+    // if (endOfString === target) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+    endOfString == target ? true : false
+    return endOfString == target
 }
 
 console.log(confirmEnding("Merhaba dünya", "dünya"))
