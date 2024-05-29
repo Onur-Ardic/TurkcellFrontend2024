c8e2c8cd674627c82835c245c11da4db528ed8d3
Elif Can <76632280+ElifCan7@users.noreply.github.com>
2024-05-29 23:17:39 +0300

    weather-app

---
+++ Ogrenciler/ElifCan/weather-hw/weather-project/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import WeatherPage from "./components/WeatherPage";
+
+function App() {
+  return (
+    <div>
+      <WeatherPage />
+    </div>
+  );
+}
+
+export default App;
