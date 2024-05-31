3e23ccb9f13366d73e2f6b2b3254586fba99027b
Semih Yücel <smhycl2341@gmail.com>
2024-05-31 12:22:27 +0300

    odev update

--- Ogrenciler/SemihYucel/GrupOdevleri/Odev18-React-HavaDurumu/hava-durumu/src/components/WeatherCard.jsx
+++ Ogrenciler/SemihYucel/GrupOdevleri/Odev18-React-HavaDurumu/hava-durumu/src/components/WeatherCard.jsx
@@ -7,7 +7,7 @@ const WeatherCard = ({ weather, city }) => {
         <p className="h2 mb-1" id="wrapper-name">{city}</p>
         <p className="mb-1" id="wrapper-description">{weather[0].description}</p>
         <p className="display-1 mb-1" id="wrapper-temp">{Math.round(weather[0].degree)}°C</p>
-        <span>Pressure: <span id="wrapper-pressure">{weather[0].min}</span></span>
+        <span>En Düşük: <span id="wrapper-pressure">{weather[0].min}</span></span>
         <span className="mx-2">|</span>
         <span>Nem: <span id="wrapper-humidity">{weather[0].humidity}</span>%</span>
       </div>
