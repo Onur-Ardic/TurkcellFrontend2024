41eafce8b9342ffd232a612f29293b4ee11a83a6
Seyma Bayezit <163021404+seymabyzt@users.noreply.github.com>
2024-06-03 02:34:47 +0300

    edit css

--- Ogrenciler/ElifCan/github/github-project/src/components/atom/button/styled.jsx
+++ Ogrenciler/ElifCan/github/github-project/src/components/atom/button/styled.jsx
@@ -1,6 +1,12 @@
 import styled from "styled-components";
 
 export const GetButton = styled.button`
-  background-color: red;
-  border-radius: 25px;
+  background-color: black;
+  border-radius: 8px;
+  border: none;
+  color: white;
+  padding: 5px;
+  &:hover {
+    background-color: purple;
+  }
 `;
