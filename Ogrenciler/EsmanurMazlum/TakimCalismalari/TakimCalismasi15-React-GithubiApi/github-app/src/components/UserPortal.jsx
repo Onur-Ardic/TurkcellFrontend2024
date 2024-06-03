3bc823cc135fec497dbe16e22e89ddd7ed23525d
Erkan TOKMAK <erkan.tokmak.ofc@gmail.com>
2024-06-03 13:18:32 +0300

    githubApp updated

--- Ogrenciler/EsmanurMazlum/TakimCalismalari/TakimCalismasi15-React-GithubiApi/github-app/src/components/UserPortal.jsx
+++ Ogrenciler/EsmanurMazlum/TakimCalismalari/TakimCalismasi15-React-GithubiApi/github-app/src/components/UserPortal.jsx
@@ -1,30 +1,37 @@
-import { UsrModal } from "../styled";
-const UserPortal = ({ userDetail, closePortal }) => {
+import { UsrModal,ButtonDetails } from "../styled";
+import Errors from "./Errors";
+const UserPortal = ({ userDetail, closePortal, error }) => {
     return (
         <>
-            {console.log(userDetail)}
             <UsrModal>
+               
                 <div className="card-header">
                     <div className="d-flex justify-content-between">
                         <h3>Profil Detay Sayfası :<strong>{userDetail?.login}</strong></h3>
                         <button className="btn-close" onClick={closePortal}></button>
                     </div>
                 </div>
-                <div className="card-body">                  
-                    
-                    <div className="row">
+                <hr/>
+                <div className="card-body mt-2">
+                    {error ? <Errors error={error} /> : <div className="row">
                         <div className="col-4 text-center">
-                        <img src={userDetail?.avatar_url} alt="avatar" width={200} height={200} className="rounded-circle" />
-                        <p><strong>{userDetail?.name}</strong></p>
-                        <p>{userDetail?.location}</p>
+                            <img src={userDetail?.avatar_url} alt="avatar" width={200} height={200} className="rounded-circle" />
+                            <h1><strong>{userDetail?.name}</strong></h1>
+                            <p>{userDetail?.location}</p>
                         </div>
                         <div className="col-8">
-                        <p>Hakkımda:{userDetail?.bio}</p>
-                        <p>Çalışmalarım:{userDetail?.public_repos}</p>
-                        <p>Takipçilerim:{userDetail?.followers}</p>
-                        <p>Takip ettiklerim:{userDetail?.following}</p>
+                            <div className="row mb-3">
+                                <div className="col-3 bg-dark bg-opacity-50 text-white rounded-5 m-3 p-3"><h3 className="text-center">Çalışmalarım <br /><span className="fs-1">{userDetail?.public_repos}</span></h3></div>
+                                <div className="col-3 bg-dark bg-opacity-50 text-white rounded-5 m-3 p-3"><h3 className="text-center">Takipçilerim <br /><span className="fs-1">{userDetail?.followers}</span></h3></div>
+                                <div className="col-3 bg-dark bg-opacity-50 text-white rounded-5 m-3 p-3"><h3 className="text-center">Takip ettiklerim <br /><span className="fs-1">{userDetail?.following}</span></h3></div>
+                            </div>
+                            <h4>{userDetail?.bio}</h4>
+                            <div className="row">
+                                <a href={userDetail?.html_url} target="_blank"><ButtonDetails>Github'a git</ButtonDetails></a>
+                            </div>
                         </div>
-                    </div>
+                    </div>}
+
 
                 </div>
             </UsrModal>
