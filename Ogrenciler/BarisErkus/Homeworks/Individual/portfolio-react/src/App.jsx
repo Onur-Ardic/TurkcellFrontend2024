2fd72b26457256c8a2e21a58551d7bcf29b54945
sarvanss <thesarvanss@gmail.com>
2024-06-05 18:00:05 +0300

    .

--- Ogrenciler/BarisErkus/Homeworks/Individual/portfolio-react/src/App.jsx
+++ Ogrenciler/BarisErkus/Homeworks/Individual/portfolio-react/src/App.jsx
@@ -1,28 +1,51 @@
+import React, { lazy, Suspense } from "react";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@material-tailwind/react";
 import "./App.css";
-import AboutMe from "./components/AboutMe";
-import AcademicInfo from "./components/Academic/Info";
-import Works from "./components/Academic/Works";
-import Home from "./components/Home";
-import Navbar from "./components/Navbar";
+import Hero from "./Components/Hero";
+import Social from "./Components/Social";
+import Navigation from "./Components/Navigation";
+import Loader from "./Components/Loader";
+import { Analytics } from "@vercel/analytics/react";
+
+const Skills = lazy(() => import("./Components/Skills/Skills"));
+const About = lazy(() => import("./Components/About"));
+const Projects = lazy(() => import("./Components/Projects/Projects"));
+const Experience = lazy(() => import("./Components/Experience/Experience"));
+const Contact = lazy(() => import("./Components/Contact"));
 
 function App() {
-  const email = "1bariserkus@gmail.com";
-
-  const socialMediaLinks = {
-    github: "https://github.com/1BarisErkus",
-    linkedin: "https://www.linkedin.com/in/bariserkus/",
-    medium: "https://medium.com/@baris.erkus",
-  };
-
   return (
-    <>
-      <Navbar email={email} />
-      <Home socialMediaLinks={socialMediaLinks} email={email} />
-      <AboutMe />
-      <AcademicInfo />
-      <Works />
-    </>
+    <ThemeProvider>
+      <div className="Hero">
+        <Analytics />
+        <Suspense fallback={<Loader />}>
+          <Outlet />
+        </Suspense>
+        <Navigation />
+      </div>
+    </ThemeProvider>
   );
 }
 
-export default App;
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    exact: true,
+    element: <App />,
+    children: [
+      { path: "/", exact: true, element: <Hero /> },
+      { path: "/about", exact: true, element: <About /> },
+      { path: "/skills", exact: true, element: <Skills /> },
+      { path: "/projects", exact: true, element: <Projects /> },
+      { path: "/experience", exact: true, element: <Experience /> },
+      { path: "/contact", exact: true, element: <Contact /> },
+    ],
+  },
+]);
+
+const Root = () => {
+  return <RouterProvider router={appRouter} />;
+};
+
+export default Root;
