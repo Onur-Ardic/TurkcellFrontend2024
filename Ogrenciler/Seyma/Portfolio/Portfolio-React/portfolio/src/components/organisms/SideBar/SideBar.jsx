c2e608c934a4cf2d9f1c8f325675371946ad1a0a
Seyma Bayezit <163021404+seymabyzt@users.noreply.github.com>
2024-06-05 13:36:18 +0300

    portfolio

--- Ogrenciler/Seyma/Portfolio/Portfolio-React/portfolio/src/components/organisms/SideBar/SideBar.jsx
+++ Ogrenciler/Seyma/Portfolio/Portfolio-React/portfolio/src/components/organisms/SideBar/SideBar.jsx
@@ -27,12 +27,12 @@ export const SideBar = ({data}) => {
       <div className='col-6'>
       <h2 className='mt-4 text-white'>Social Accounts</h2>
       <p className='text-white'>See my profiles on social platforms. You can reach me from these platforms.</p>
-      <a className='button' href="https://github.com/seymabyzt">Discover</a>
+      <a className='button' href="https://github.com/seymabyzt" target='_blank'>Discover</a>
       <div className='social gap-2'>
-        <div className='socialLogo'onMouseOver={handleHover} ><img src="https://r.resimlink.com/uJ2XnhpqL.png" alt="" /></div>
-        <div className='socialLogo' ><img src="https://r.resimlink.com/rRaKU-W.png" alt="" /></div>
-        <div className='socialLogo'><img src="https://r.resimlink.com/UJ5dhoHyjbL.png" alt="" /></div>
-        <div className='socialLogo'  ><img src="https://r.resimlink.com/0EldvkM.png" alt="" /></div>
+        <a className='socialLogo'onMouseOver={handleHover}target='_blank' ><img src="https://r.resimlink.com/uJ2XnhpqL.png" alt="" /></a>
+        <a target='_blank' href='https://medium.com/@bayezittseyma' className='socialLogo' ><img src="https://r.resimlink.com/rRaKU-W.png" alt="" /></a>
+        <a target='_blank' href='https://www.linkedin.com/in/seyma-bayezit-009509170/' className='socialLogo'><img src="https://r.resimlink.com/UJ5dhoHyjbL.png" alt="" /></a>
+        <a target='_blank' href='https://www.behance.net/eymabayezit' className='socialLogo'  ><img src="https://r.resimlink.com/0EldvkM.png" alt="" /></a>
       </div>
       </div>
     <div className='col-6' style = {displayBlock}>
