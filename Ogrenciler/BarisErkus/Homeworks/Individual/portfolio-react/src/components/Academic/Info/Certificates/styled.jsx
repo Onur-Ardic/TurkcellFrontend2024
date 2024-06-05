188d565230831b2cfbad23b8783b40768540849b
Barış Erkuş <1bariserkus@gmail.com>
2024-06-05 20:14:10 +0300

    last build

--- Ogrenciler/BarisErkus/Homeworks/Individual/portfolio-react/src/components/Academic/Info/Certificates/styled.jsx
+++ Ogrenciler/BarisErkus/Homeworks/Individual/portfolio-react/src/components/Academic/Info/Certificates/styled.jsx
@@ -2,13 +2,17 @@ import styled from "styled-components";
 import { tertiary } from "../../../../common/colors";
 
 export const A = styled.a`
-  font-size: 1.5rem;
+  font-size: 1.25rem;
   transition: 0.3s;
+  font-weight: 500;
   &:hover {
     color: ${tertiary};
   }
   &:focus {
     outline: 2px solid ${tertiary};
     outline-offset: 8px;
+  }
+  @media (max-width: 768px) {
+    font-size: 1rem;
   }
 `;
