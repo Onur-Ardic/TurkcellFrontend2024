36a7066d1e891eb40aac253083161e1e36f11e29
Barış Erkuş <1bariserkus@gmail.com>
2024-06-07 20:01:47 +0300

    lesson 21

---
+++ Ogrenciler/BarisErkus/Lessons/lesson-21-router/src/views/ParametreView.jsx
@@ -0,0 +1,9 @@
+import { useParams } from "react-router-dom";
+
+const ParametreView = () => {
+  const { id } = useParams();
+
+  return <div>ParametreView {id}</div>;
+};
+
+export default ParametreView;
