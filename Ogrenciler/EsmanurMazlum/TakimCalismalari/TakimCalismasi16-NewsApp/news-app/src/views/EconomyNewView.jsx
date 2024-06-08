2aa84ed6dedae22ad411700638c276cc6258c9b2
Necati Doğrul <necatidogrul7@gmail.com>
2024-06-08 19:05:18 +0300

    react news api odev

--- Ogrenciler/EsmanurMazlum/TakimCalismalari/TakimCalismasi16-NewsApp/news-app/src/views/EconomyNewView.jsx
+++ Ogrenciler/EsmanurMazlum/TakimCalismalari/TakimCalismasi16-NewsApp/news-app/src/views/EconomyNewView.jsx
@@ -1,20 +1,42 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Requests from "../Requests";
 
 const EconomyNewView = () => {
+  const [economyNews, setEconomyNews] = useState([]);
 
-    const getNews = () => {
-        Requests.get("business")
-            .then(data => console.log(data)).catch(err => console.log(err));
-    }
+  const getNews = () => {
+    Requests.get("business")
+      .then((response) => {
+        if (response.status === "ok") {
+          setEconomyNews(response.articles);
+        } else {
+          console.error("Beklenmeyen veri formatı:", response);
+          setEconomyNews([]);
+        }
+      })
+      .catch((err) => console.log(err));
+  };
 
-    useEffect(() => {
-        getNews();
-      }, []);
+  useEffect(() => {
+    getNews();
+  }, []);
 
   return (
-    <div>EconomyNewView</div>
-  )
-}
+    <div>
+      <h1>Economy News</h1>
+      {economyNews.length > 0 ? (
+        economyNews.map((item) => (
+          <div key={item.id}>
+            <h3>{item.title}</h3>
+            <p>{item.description}</p>
+            <p>{item.author}</p>
+          </div>
+        ))
+      ) : (
+        <p>Yüklenecek ekonomi haberi bulunamadı</p>
+      )}
+    </div>
+  );
+};
 
-export default EconomyNewView
+export default EconomyNewView;
--- Ogrenciler/EsmanurMazlum/TakimCalismalari/TakimCalismasi16-NewsApp/news-app/src/views/SportNewView.jsx
+++ Ogrenciler/EsmanurMazlum/TakimCalismalari/TakimCalismasi16-NewsApp/news-app/src/views/SportNewView.jsx
@@ -1,21 +1,42 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Requests from "../Requests";
 
 const SportNewView = () => {
+  const [sportNews, setSportNews] = useState([]);
 
-    const getNews = () => {
-        Requests.get("sport")
-            .then(data => console.log(data)).catch(err => console.log(err));
-    }
+  const getNews = () => {
+    Requests.get("sport")
+      .then((response) => {
+        if (response.status === "ok") {
+          setSportNews(response.articles);
+        } else {
+          console.error("Beklenmeyen veri formatı:", response);
+          setSportNews([]);
+        }
+      })
+      .catch((err) => console.log(err));
+  };
 
-    useEffect(() => {
-        getNews();
-      }, []);
-
+  useEffect(() => {
+    getNews();
+  }, []);
 
   return (
-    <div>sport sayfası</div>
-  )
-}
+    <div>
+      <h1>Sport News</h1>
+      {sportNews.length > 0 ? (
+        sportNews.map((item) => (
+          <div key={item.id}>
+            <h3>{item.title}</h3>
+            <p>{item.description}</p>
+            <p>{item.author}</p>
+          </div>
+        ))
+      ) : (
+        <p>Yüklenecek spor haberi bulunamadı</p>
+      )}
+    </div>
+  );
+};
 
-export default SportNewView
+export default SportNewView;
