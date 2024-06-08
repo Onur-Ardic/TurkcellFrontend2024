509a37a49ac289f855cad5c5e470fd1343ee5f36
Recep <49814995+Recepurkun@users.noreply.github.com>
2024-06-08 16:53:36 +0300

    fixed

--- Ogrenciler/EsmanurMazlum/TakimCalismalari/TakimCalismasi16-NewsApp/news-app/src/views/HomePage.jsx
+++ Ogrenciler/EsmanurMazlum/TakimCalismalari/TakimCalismasi16-NewsApp/news-app/src/views/HomePage.jsx
@@ -1,24 +1,25 @@
 import { useEffect } from "react";
 import Requests from "../Requests";
 
-
 function HomePage() {
+  // const getNews = () => {
+  //     // Requests.get("https://newsapi.org/v2/top-headlines/sources?country=trapiKey=223d61cd6e5a43d683afc6f5405454d3")
+  //         .then(data => console.log(data)).catch(err => console.log(err));
+  // }
 
-    const getNews = () => {
-        Requests.get("https://newsapi.org/v2/top-headlines/sources?country=trapiKey=223d61cd6e5a43d683afc6f5405454d3")
-            .then(data => console.log(data)).catch(err => console.log(err));
-    }
+  Requests.get(
+    "https://newsapi.org/v2/top-headlines?country=tr&category=sports&apiKey=9da1e84daa1142edb097ad9e12abaeb6"
+  );
 
-    useEffect(() => {
-        getNews();
-    },[])
-
+  // useEffect(() => {
+  //   getNews();
+  // }, []);
 
   return (
     <>
-
+      <button onClick={() => getNews()}>Verileri Getir</button>
     </>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
