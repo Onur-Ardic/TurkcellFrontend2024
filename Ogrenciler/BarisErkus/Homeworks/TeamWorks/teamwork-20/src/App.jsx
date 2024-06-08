4709801d3ff173d35cda51d0bfc76e89d8c315e0
Barış Erkuş <1bariserkus@gmail.com>
2024-06-08 13:45:55 +0300

    teamwork 20 structer has been complated

---
+++ Ogrenciler/BarisErkus/Homeworks/TeamWorks/teamwork-20/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Router from "./routes/Router";
+// 1c1772bbfd4c41169acdab14b606a2ea
+function App() {
+  return (
+    <>
+      <h1>adsadsadsada</h1>
+      <Router />
+    </>
+  );
+}
+
+export default App;
