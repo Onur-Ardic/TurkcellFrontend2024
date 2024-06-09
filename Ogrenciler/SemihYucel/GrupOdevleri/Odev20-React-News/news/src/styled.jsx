823b56a37288ccb252b6b30b6ef1d8b5ebbe67ab
Semih Yücel <smhycl2341@gmail.com>
2024-06-09 12:52:12 +0300

    detail page update

--- Ogrenciler/SemihYucel/GrupOdevleri/Odev20-React-News/news/src/styled.jsx
+++ Ogrenciler/SemihYucel/GrupOdevleri/Odev20-React-News/news/src/styled.jsx
@@ -24,4 +24,42 @@ export const Card = styled.div`
     height: 250px;
     object-fit: cover;
   }
+
+  
+`
+export const DetailViewContainer = styled.div`
+  padding: 20px;
+  max-width: 900px;
+  margin: 0 auto;
+  background-color: #f9f9f9;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+`;
+
+export const Title = styled.h1`
+  font-size: 2rem;
+  color: #333;
+  margin-bottom: 10px;
+`;
+
+export const Description = styled.p`
+  font-size: 1.2rem;
+  color: #666;
+  margin-bottom: 20px;
+`;
+
+export const Author = styled.div`
+  font-size: 1rem;
+  color: #999;
+  text-align: right;
+  margin-top: 20px;
+`;
+
+export const Image = styled.img`
+  width: 100%;
+  height: auto;
+  max-height: 400px;
+  border-radius: 10px;
+  margin-bottom: 20px;
+  object-fit:cover
 `;
--- Ogrenciler/SemihYucel/GrupOdevleri/Odev20-React-News/news/src/views/DetailView.jsx
+++ Ogrenciler/SemihYucel/GrupOdevleri/Odev20-React-News/news/src/views/DetailView.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import { useLocation, useParams } from "react-router-dom";
+import { DetailViewContainer, Title, Description, Author, Image } from "../styled";
 
 const DetailView = () => {
   const { newsId } = useParams();
   const location = useLocation();
   const data = location.state;
-  console.log("detail page", data);
+  const defaultImage = "https://png.pngtree.com/png-vector/20201027/ourmid/pngtree-breaking-news-banner-lower-png-image_2378724.jpg";
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <>
-      <div>DetailView</div>
-      <div>{data.title}</div>
-      <div>{data.description}</div>
-      <div>{data.author}</div>
-    </>
+    <DetailViewContainer>
+      <Image src={data.urlToImage ? data.urlToImage : defaultImage} alt={data.title} />
+      <Title>{data.title}</Title>
+      <Description>{data.description}</Description>
+      <Author>{data.author}</Author>
+    </DetailViewContainer>
   );
 };
 
