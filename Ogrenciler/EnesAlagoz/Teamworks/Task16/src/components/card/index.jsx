ca0ec303bdf560bef708672787884985e4fa34c1
enesalagoz1212 <118561378+enesalagoz1212@users.noreply.github.com>
2024-06-10 13:46:36 +0300

    Homework done with react router

---
+++ Ogrenciler/EnesAlagoz/Teamworks/Task16/src/components/card/index.jsx
@@ -0,0 +1,27 @@
+import { CardBody, CardBtn, CardContainer, CardImg, CardSubtitle, CardTitle } from "../../StyledComponent";
+
+const Card = ({ data }) => {
+  return (
+    <CardContainer>
+      <CardImg
+        src={data.urlToImage ? data.urlToImage : "https://www.sag.at/wp-content/uploads/2020/05/news.jpg"}
+        alt="news-photo"
+      />
+      <CardBody>
+        <CardTitle>
+          {data.title === "[Removed]"
+            ? "Breaking News"
+            : data.title.length > 110
+            ? data.title.substring(0, 110) + "..."
+            : data.title}
+        </CardTitle>
+        <CardSubtitle>{data.author ? data.author : "CNBC"}</CardSubtitle>
+        <CardBtn href={data.url} target="_blank" rel="noopener noreferrer">
+          Read more
+        </CardBtn>
+      </CardBody>
+    </CardContainer>
+  );
+};
+
+export default Card;
