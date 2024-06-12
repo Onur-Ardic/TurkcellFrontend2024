4222890175a4943e9a88b78d600022bc00c5152e
aaltunbas96 <a.altunbas@outlook.com.tr>
2024-06-12 21:22:03 +0300

    Redux Update

--- Ogrenciler/Atakan/Context-Redux/Redux/src/components/ToDoCard.jsx
+++ Ogrenciler/Atakan/Context-Redux/Redux/src/components/ToDoCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { deleteTodo, updateTodo } from "../redux/slices/todoSlice";
 import Button from "./Button";
@@ -6,29 +6,39 @@ import Button from "./Button";
 const ToDoCard = () => {
   const todos = useSelector((state) => state.todo.todos);
   const dispatch = useDispatch();
+  const [editingTodoId, setEditingTodoId] = useState(null);
+  const [updatedTitle, setUpdatedTitle] = useState("");
 
   const handleDelete = (id) => {
     dispatch(deleteTodo(id));
   };
 
   const handleUpdate = (id) => {
-    dispatch(updateTodo(id));
-    setIsEditing(false);
+    dispatch(updateTodo({ id: id, title: updatedTitle }));
+    setEditingTodoId(null);
   };
+
   return (
     <>
       <ul>
         {todos.map((todo) => (
           <li key={todo.id}>
-            {todo.title} - {todo.id}
-            <Button onClick={() => handleDelete(todo.id)} text="Delete" />
-            <Button
-              onClick={() => {
-                handleUpdate(todo.id);
-                setIsEditing(true);
-              }}
-              text="Update"
-            />
+            {editingTodoId === todo.id ? (
+              <>
+                <input
+                  type="text"
+                  value={updatedTitle}
+                  onChange={(e) => setUpdatedTitle(e.target.value)}
+                />
+                <Button onClick={() => handleUpdate(todo.id)} text="Save" />
+              </>
+            ) : (
+              <>
+                {todo.title} - {todo.id}
+                <Button onClick={() => handleDelete(todo.id)} text="Delete" />
+                <Button onClick={() => setEditingTodoId(todo.id)} text="Edit" />
+              </>
+            )}
           </li>
         ))}
       </ul>
