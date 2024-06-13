b8d1567b4f80afb470eb7431b07450d0531196a9
enesalagoz1212 <118561378+enesalagoz1212@users.noreply.github.com>
2024-06-13 21:41:47 +0300

    create

---
+++ Ogrenciler/EnesAlagoz/Teamworks/Redux/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Form from "./components/Form";
+import ToDoCard from "./components/ToDoCard";
+function App() {
+  return (
+    <>
+      <h1>Redux Todo</h1>
+      <Form />
+      <ToDoCard />
+    </>
+  );
+}
+
+export default App;
