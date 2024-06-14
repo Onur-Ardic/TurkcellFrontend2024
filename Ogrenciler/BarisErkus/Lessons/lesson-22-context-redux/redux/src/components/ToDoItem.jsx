5f72b1d35a5c8a7e4ce03ee1b9b31026d6a12827
Barış Erkuş <1bariserkus@gmail.com>
2024-06-14 13:43:23 +0300

    todo with redux

--- Ogrenciler/BarisErkus/Lessons/lesson-22-context-redux/redux/src/components/ToDoItem.jsx
+++ Ogrenciler/BarisErkus/Lessons/lesson-22-context-redux/redux/src/components/ToDoItem.jsx
@@ -1,8 +1,10 @@
 import { useDispatch } from "react-redux";
 import { deleteTodo, setTodo } from "../redux/slices/todoSlice";
+import StarRating from "bstarrating";
+import { useState } from "react";
 
 const ToDoItem = ({ todo }) => {
-  console.log(todo);
+  const [star, setStar] = useState(0);
 
   const dispatch = useDispatch();
 
@@ -15,10 +17,39 @@ const ToDoItem = ({ todo }) => {
   };
 
   return (
-    <li>
+    <li
+      style={{
+        backgroundColor:
+          star === 0
+            ? "rgb(236, 213, 181)"
+            : star === 1
+            ? "#C25A47"
+            : star === 2
+            ? "#2FBBCB"
+            : "#48B193",
+      }}
+    >
       {todo.title}
-      <button onClick={handleUpdate}>Güncelle</button>
-      <button onClick={() => handleDelete(todo.id)}>Sil</button>
+      <div>
+        <StarRating
+          size={30}
+          className="myStar"
+          messages={["", "", ""]}
+          onSetRating={(value) => {
+            setStar(value);
+            alert(
+              value === 1
+                ? "Ama yine de iyi ilerledim."
+                : value === 2
+                ? "Çok iyi ilerledim."
+                : "Çok çok iyi ilerledim."
+            );
+          }}
+          maxRating={3}
+        />
+        <button onClick={handleUpdate}>Güncelle</button>
+        <button onClick={() => handleDelete(todo.id)}>Sil</button>
+      </div>
     </li>
   );
 };
