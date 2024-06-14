908cac13e80bf794856ab337240bb92ae49272b2
Fatih <fdonmez_1512@hotmail.com>
2024-06-14 10:18:53 +0300

    remove unnecessary functions

--- Ogrenciler/Fatih-Donmez/Homeworks/React-TodoApp-Redux-Toolkit/src/App.jsx
+++ Ogrenciler/Fatih-Donmez/Homeworks/React-TodoApp-Redux-Toolkit/src/App.jsx
@@ -10,46 +10,6 @@ import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 
 function App() {
-  const todos = useSelector((state) => state.todo.todos);
-  const dispatch = useDispatch();
-  const [newTodo, setNewTodo] = useState("");
-  const [editMode, setEditMode] = useState(null);
-  const [editTitle, setEditTitle] = useState("");
-
-  const handleDelete = (id) => {
-    dispatch(reduxDeleteTodo(id));
-  };
-
-  const handleAddTodo = () => {
-    if (newTodo.trim()) {
-      const id = new Date().getTime();
-      dispatch(
-        reduxAddTodo({
-          id,
-          title: newTodo,
-        })
-      );
-      setNewTodo("");
-    }
-  };
-
-  const handleEdit = (todo) => {
-    setEditMode(todo.id);
-    setEditTitle(todo.title);
-  };
-
-  const handleUpdateTodo = (id) => {
-    if (editTitle.trim()) {
-      dispatch(
-        reduxUpdateTodo({
-          id,
-          title: editTitle,
-        })
-      );
-      setEditMode(null);
-      setEditTitle("");
-    }
-  };
   return (
     <div className="container mt-5">
       <h1 className="text-center mb-4">Redux Todo App</h1>
