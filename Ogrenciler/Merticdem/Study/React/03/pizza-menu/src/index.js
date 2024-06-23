2f90140b63b426c141118974a082f7f8ab137ac0
merticdem <merticdem@hotmail.com>
2024-06-23 21:21:54 +0300

    dd

--- Ogrenciler/Merticdem/Study/React/03/pizza-menu/src/index.js
+++ Ogrenciler/Merticdem/Study/React/03/pizza-menu/src/index.js
@@ -1,19 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
 
-function App() {
-  return (
-    <div>
-      <h1>Pizza Menu</h1>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div>
+//       <h1>Pizza Menu</h1>
+//     </div>
+//   );
+// }
 
-const root = ReactDom.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDom.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
-// React.render(<App />, document.getElementById("root"));
+// // React.render(<App />, document.getElementById("root"));
