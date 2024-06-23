1677d20a4a276b85c4b3139f327732bcc0e72d46
merticdem <merticdem@hotmail.com>
2024-06-23 21:52:51 +0300

    updated
    updated modal

--- Ogrenciler/Merticdem/Uygulamalar/React-to-do-listt/to-do-redux/src/App.jsx
+++ Ogrenciler/Merticdem/Uygulamalar/React-to-do-listt/to-do-redux/src/App.jsx
@@ -1,27 +1,50 @@
 import "./App.css";
 import { useSelector, useDispatch } from "react-redux";
+import { useState } from "react";
 import { addTodo, deleteTodo, updateTodo } from "./redux/slices/todoSlice";
+
 function App() {
   const todos = useSelector((state) => state.todo.todos);
   const dispatch = useDispatch();
+  const [showModal, setShowModal] = useState(false);
+  const [currentTodo, setCurrentTodo] = useState(null);
+  const [newTitle, setNewTitle] = useState("");
 
   const handleDelete = (id) => {
     dispatch(deleteTodo(id));
   };
+
   const handleAddTodo = (todo) => {
     dispatch(addTodo(todo));
   };
-  const handleEdit = (id, title) => {
-    const newTitle = prompt("Enter new title", title);
-    if (newTitle) {
+
+  const openModal = (todo) => {
+    setCurrentTodo(todo);
+    setNewTitle(todo.title);
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+    setCurrentTodo(null);
+  };
+
+  const handleTitleChange = (e) => {
+    setNewTitle(e.target.value);
+  };
+
+  const handleUpdateTodo = () => {
+    if (currentTodo) {
       dispatch(
         updateTodo({
-          id,
+          id: currentTodo.id,
           title: newTitle,
         })
       );
+      closeModal();
     }
   };
+
   return (
     <>
       <h1>Redux Todo</h1>
@@ -30,9 +53,10 @@ function App() {
           e.preventDefault();
           const todo = e.target.elements.todo.value;
           handleAddTodo({ id: self.crypto.randomUUID(), title: todo });
+          e.target.reset(); // Clear the input field
         }}
       >
-        <input type="text" name="todo" />
+        <input type="text" name="todo" required />
         <button type="submit">Add Todo</button>
       </form>
       <ul>
@@ -40,12 +64,19 @@ function App() {
           <li key={todo.id}>
             {todo.title} - {todo.id}
             <button onClick={() => handleDelete(todo.id)}>Delete</button>
-            <button onClick={() => handleEdit(todo.id, todo.title)}>
-              Edit
-            </button>
+            <button onClick={() => openModal(todo)}>Edit</button>
           </li>
         ))}
       </ul>
+      {showModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <input type="text" value={newTitle} onChange={handleTitleChange} />
+            <button onClick={handleUpdateTodo}>Update</button>
+            <button onClick={closeModal}>Cancel</button>
+          </div>
+        </div>
+      )}
     </>
   );
 }
