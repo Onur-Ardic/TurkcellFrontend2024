a5e17228611677faf829755d0ddd3a2e9c2282fc
merticdem <merticdem@hotmail.com>
2024-06-25 17:08:36 +0300

    next
    teamwork completed

--- Ogrenciler/Merticdem/Uygulamalar/React-Next-blog/next-blog/src/app/blog/[[...id]]/page.jsx
+++ Ogrenciler/Merticdem/Uygulamalar/React-Next-blog/next-blog/src/app/blog/[[...id]]/page.jsx
@@ -1,30 +1,31 @@
 "use client";
 import { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import Link from "next/link";
 
-const BlogPage = () => {
+const BlogPage = ({ params }) => {
   const [data, setData] = useState([]);
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
+    fetch(`https://jsonplaceholder.typicode.com/posts/${params.id}`)
       .then((res) => res.json())
       .then((data) => {
         setData(data);
-        console.log(data);
+        console.log(params.id);
       });
   }, []);
 
   return (
     <div>
-      BLogid
-      {data.map((item) => (
-        <div key={item.id}>
-          <img
-            src={`https://picsum.photos/id/${Math.floor(
-              Math.random() * 1000
-            )}/200/300`}
-          />
-          <div>{item.title}</div>
+      <div className="container text-center mt-3">
+        <Link href={"/"}>ANASAYFA</Link>
+        <div className="imageContainer">
+          <img src={`https://picsum.photos/id/${data.id}/200/300`} />
         </div>
-      ))}
+        <div className="contentContainer">
+          <h1>{data.title}</h1>
+          <p>{data.body}</p>
+        </div>
+      </div>
     </div>
   );
 };
