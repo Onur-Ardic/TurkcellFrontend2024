3ce197274bebf8bac41a651607375eb4fe043392
Veli Dogan <velidogan120@gmail.com>
2024-06-25 23:34:53 +0300

    PostApp Finished

---
+++ Ogrenciler/velidogan/Tasks/post-app/src/app/posts/[[...post]]/page.jsx
@@ -0,0 +1,37 @@
+"use client";
+import Request from "@/app/request";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+const Post = ({params}) => {
+  const [post,setPost] = useState({});
+  const request =async () => {
+    const response = await Request.get("https://jsonplaceholder.typicode.com/posts/" + params.post);
+    return setPost(response);
+  }
+  useEffect(()=>{
+    request();
+  },[]);
+  return (
+    <div className="container py-4 my-3 bg-dark text-light rounded-4" style={{maxWidth:"400px"}}>
+      <nav aria-label="breadcrumb">
+        <ol className="breadcrumb">
+          <li className="breadcrumb-item"><Link href="/" className="text-decoration-none">Home</Link></li>
+          <li className="breadcrumb-item"><Link href="/" className="text-decoration-none">Posts</Link></li>
+          <li className="breadcrumb-item active text-light" aria-current="page">Post {post.id}</li>
+        </ol>
+      </nav>
+      <div className="card rounded-3">
+            <Image className="rounded-top-3 card-img" src={`https://picsum.photos/id/${post.id+100}/200/300`} alt="Blog Resmi" width={200} height={300}/>
+            <div className="card-body">
+              <h1>{post.id}</h1>
+              <h5 className="card-title text-uppercase fw-semibold">{post.title}</h5>
+              <p className="card-text fst-italic">{post.body}</p>
+            </div>
+      </div>
+    </div>
+  )
+}
+
+export default Post
