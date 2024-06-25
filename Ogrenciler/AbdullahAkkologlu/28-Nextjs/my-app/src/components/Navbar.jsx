ee67adea3f0c426d68ed842d56a13fad9dbf38c9
Esinn <eoruc369@gmail.com>
2024-06-25 21:55:04 +0300

    next.js blog project - global.css has been updated.

--- Ogrenciler/AbdullahAkkologlu/28-Nextjs/my-app/src/components/Navbar.jsx
+++ Ogrenciler/AbdullahAkkologlu/28-Nextjs/my-app/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-light bg-light ">
+    <nav className="navbar navbar-light bg-white mb-3 border-bottom w-75 mx-auto">
       <a
         className="navbar-brand mx-auto fs-1 d-block w-100 text-center pt-3"
         href="#"
