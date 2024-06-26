fc872197cb624717496e931280408ceac249b820
Abdullah Akkoloğlu <abdullahakkol@gmail.com>
2024-06-26 13:21:12 +0300

    blogCard component added to detail page

--- Ogrenciler/AbdullahAkkologlu/28-Nextjs/my-app/src/app/page.jsx
+++ Ogrenciler/AbdullahAkkologlu/28-Nextjs/my-app/src/app/page.jsx
@@ -1,6 +1,7 @@
 import BlogCard from "@/components/BlogCard";
 import { getBlogs } from "@/service/api";
 import classes from "./page.module.css";
+import Link from "next/link";
 
 const Blogs = async () => {
   const data = await getBlogs();
@@ -8,9 +9,13 @@ const Blogs = async () => {
     <div className="container">
       <div className="row">
         {data.slice(9, 21).map((blog) => (
-          <div key={blog.id} className={`col-lg-4 ${classes.card}`}>
+          <Link
+            href={`blog/${blog.id}`}
+            key={blog.id}
+            className={`col-lg-4 ${classes.card}`}
+          >
             <BlogCard blog={blog} />
-          </div>
+          </Link>
         ))}
       </div>
     </div>
