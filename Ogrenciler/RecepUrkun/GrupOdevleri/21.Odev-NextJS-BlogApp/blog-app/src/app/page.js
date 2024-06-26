e90b5ed1a9b01256cf3516d3694c7b327473cbda
Bugra034 <Bugraozk58@gmail.com>
2024-06-26 20:33:10 +0300

    blogapp update

--- Ogrenciler/RecepUrkun/GrupOdevleri/21.Odev-NextJS-BlogApp/blog-app/src/app/page.js
+++ Ogrenciler/RecepUrkun/GrupOdevleri/21.Odev-NextJS-BlogApp/blog-app/src/app/page.js
@@ -2,14 +2,20 @@ import Footer from "@/components/Footer/Footer";
 import BlogCard from "@/components/BlogCard/BlogCard";
 import { getPosts } from "@/api/api";
 
+const delay = async (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
 
 export default async function Home() {
-  const data = await getPosts()
+  await delay(2000);
+  const data = await getPosts();
 
   return (
-    <div className="container" >
+    <div className="container">
       <div className="row">
-        {data.slice(0, 10).map((post) => <BlogCard key={post.id} post={post} />)}
+        {data.slice(0, 10).map((post) => (
+          <BlogCard key={post.id} post={post} />
+        ))}
       </div>
       <Footer />
     </div>
