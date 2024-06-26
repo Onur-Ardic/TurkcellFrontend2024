4c04a54a03ae7feec25ee41f0ecbe2ff8ad00533
Barış Erkuş <1bariserkus@gmail.com>
2024-06-26 18:49:54 +0300

    last

---
+++ Ogrenciler/BarisErkus/Homeworks/TeamWorks/teamwork-21/project/src/app/not-found.js
@@ -0,0 +1,17 @@
+"use client";
+const NotFound = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      NotFound
+    </div>
+  );
+};
+
+export default NotFound;
