8cbb8febeefd32494b8118d76e9199b50836b217
enesalagoz1212 <118561378+enesalagoz1212@users.noreply.github.com>
2024-06-26 17:52:31 +0300

    next js task create

---
+++ Ogrenciler/EnesAlagoz/Teamworks/Odev20-Next/my-blog/src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav class="navbar navbar-expand-lg bg-warning">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          BLOG
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#blogNavbar"
+          aria-controls="blogNavbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <Link className="nav-link" href="/">
+                Home
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
