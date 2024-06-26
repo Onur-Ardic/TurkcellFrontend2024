67fb3ccd95d5bbe900b40f6787d80d1c75aa6c15
Abdullah Akkoloğlu <abdullahakkol@gmail.com>
2024-06-26 13:46:28 +0300

    yorum satırları ve gereksiz dosyalar kaldırıldı

--- Ogrenciler/AbdullahAkkologlu/28-Nextjs/my-app/src/app/page.jsx
+++ Ogrenciler/AbdullahAkkologlu/28-Nextjs/my-app/src/app/page.jsx
@@ -1,6 +1,5 @@
 import BlogCard from "@/components/BlogCard";
 import { getBlogs } from "@/service/api";
-import classes from "./page.module.css";
 import Link from "next/link";
 
 const Blogs = async () => {
@@ -9,11 +8,7 @@ const Blogs = async () => {
     <div className="container">
       <div className="row">
         {data.slice(9, 21).map((blog) => (
-          <Link
-            href={`blog/${blog.id}`}
-            key={blog.id}
-            className={`col-lg-4 ${classes.card}`}
-          >
+          <Link href={`blog/${blog.id}`} key={blog.id} className="col-lg-4">
             <BlogCard blog={blog} />
           </Link>
         ))}
