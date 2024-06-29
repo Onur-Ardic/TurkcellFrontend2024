f93a5bb321891db1e0957e31e312be33e4f04c0c
Hasan YASEN <ysnhasan1@gmail.com>
2024-06-29 17:39:39 +0300

    exercise

---
+++ Ogrenciler/HasanYasen/Formik-Yup/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header"
+import InternshipForm from "./components/InternshipForm"
+
+function App() {
+  return (
+    <>
+      <Header />
+      <InternshipForm />
+    </>
+  )
+}
+
+export default App
