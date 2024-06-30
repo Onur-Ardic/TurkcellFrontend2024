a9bfee62db3dda9ffa26fd550ee940455886c21f
karakayaezgi <karakayaezgi620@gmail.com>
2024-06-30 17:06:33 +0300

    next-giris notes added

---
+++ Ogrenciler/Ezgi/Ders-Notlari/Next-giris/my-app/src/app/about/layout.jsx
@@ -0,0 +1,10 @@
+const aboutLayout = ({children}) => {
+  return (
+    <div>
+      <h1>About Layout Bölümü about ın altındaki her yerde görünecek...</h1>
+      {children}
+    </div>
+  )
+}
+
+export default aboutLayout
---
+++ Ogrenciler/Ezgi/Ders-Notlari/Next-giris/my-app/src/app/blog/[[...id]]/page.jsx
@@ -0,0 +1,9 @@
+const BlogItem = ({params}) => {
+  return (
+    <div>
+      Blog ID: {params.id}
+    </div>
+  )
+}
+
+export default BlogItem
