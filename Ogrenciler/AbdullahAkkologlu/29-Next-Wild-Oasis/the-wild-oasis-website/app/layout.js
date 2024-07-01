f4b83723f6fd2d0089e7378d704bcf7b5fabb58c
Abdullah Akkoloğlu <abdullahakkol@gmail.com>
2024-07-01 15:48:57 +0300

    styling

--- Ogrenciler/AbdullahAkkologlu/29-Next-Wild-Oasis/the-wild-oasis-website/app/layout.js
+++ Ogrenciler/AbdullahAkkologlu/29-Next-Wild-Oasis/the-wild-oasis-website/app/layout.js
@@ -1,6 +1,11 @@
-import Logo from "@/app/_components/Logo";
-import Navigation from "@/app/_components/Navigation";
 import "@/app/_styles/globals.css";
+import { Josefin_Sans } from "next/font/google";
+import Header from "./_components/Header";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata = {
   // title: "The Wild Oasis",
@@ -15,13 +20,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-50 min-h-screen">
+      <body
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-50 min-h-screen flex flex-col relative`}
+      >
         <header>
-          <Logo />
-          <Navigation />
+          <Header />
         </header>
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
+        </div>
       </body>
     </html>
   );
