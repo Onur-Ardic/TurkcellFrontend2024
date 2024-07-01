8abe8d917158d15f062da4694b0ce051f8f03919
Semih Yücel <smhycl2341@gmail.com>
2024-07-01 18:39:25 +0300

    yup formik

---
+++ Ogrenciler/SemihYucel/GrupOdevleri/Odev24-Next-stajformu/stajformu/src/app/pages/index.js
@@ -0,0 +1,10 @@
+import InternshipForm from "./components/InternshipForm";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Staj Başvuru Formu</h1>
+      <InternshipForm />
+    </div>
+  );
+}
