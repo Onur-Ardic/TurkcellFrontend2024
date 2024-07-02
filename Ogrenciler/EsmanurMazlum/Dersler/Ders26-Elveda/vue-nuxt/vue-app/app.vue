a245888f007fe559759b9dc2a197d50a5c253bc4
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-07-02 22:13:35 +0300

    vue nuxt  projesi eklendi

---
+++ Ogrenciler/EsmanurMazlum/Dersler/Ders26-Elveda/vue-nuxt/vue-app/app.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>Todo App</h1>
+    <form @submit.prevent="addTodo">
+      <div class="field">
+        <div class="control">
+          <input v-model="todo" type="text" placeholder="Todo Gir" class="input"/>
+        </div>
+        <button type="submit">Ekle</button>
+      </div>
+    </form>
+    <div v-for="todo in todos" :key="todo.id">
+      <div class="card-content cursor" :class="{ done: todo.done }" @click="done(todo)">
+        {{ todo.content }} - {{ todo.done }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    setup(){
+      const todo = ref("");
+      const todos = ref([]);
+
+      function addTodo(){
+        todos.value.push({
+          done: false,
+          content: todo.value,
+          id: Date.now()        
+        })
+      }
+    }
+  }
+
+
+</script>
