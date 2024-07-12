03526788a96a270ef6de2f69d9a1e042db396650
Onur Ardıç <onurardc@outlook.com>
2024-07-12 16:14:11 +0300

    error design

--- Ogrenciler/OnurArdic/Odevler/Final/src/app/[locale]/error.js
+++ Ogrenciler/OnurArdic/Odevler/Final/src/app/[locale]/error.js
@@ -1,14 +1,16 @@
 'use client'
 
 import { useEffect } from 'react'
+import './(Sites)/styles/error.scss'
+import toast from 'react-hot-toast'
 
 export default function Error({ error, reset }) {
   useEffect(() => {
-    console.error(error)
+    toast.error(error)
   }, [error])
 
   return (
-    <div>
+    <div className="error-container">
       <h2>Something went wrong!</h2>
       <button onClick={() => reset()}>Try again</button>
     </div>
