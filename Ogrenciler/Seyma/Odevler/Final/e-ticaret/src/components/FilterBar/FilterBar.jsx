db97051524032a79eb485b286b39967714578e21
Seyma Bayezit <163021404+seymabyzt@users.noreply.github.com>
2024-07-12 16:32:27 +0300

    edit

--- Ogrenciler/Seyma/Odevler/Final/e-ticaret/src/components/FilterBar/FilterBar.jsx
+++ Ogrenciler/Seyma/Odevler/Final/e-ticaret/src/components/FilterBar/FilterBar.jsx
@@ -17,9 +17,8 @@ const FilterBar = () => {
         <div className='filterBar'>
           <div className='d-flex justify-content-between'><h4  className="filterTitle">Filters</h4>
           <Image src={filtericon}></Image></div>
-            
-            <div className='categories'>
-                <ul className='list-unstyled mb-2'>
+            <div className='categories mb-3'>
+                <ul className='list-unstyled mb-3 row gap-2'>
                     <li className='d-flex justify-content-between ctgSubTitle'><span>Tshirts</span> <i className="bi bi-chevron-right"></i></li>
                     <li className='ctgSubTitle d-flex justify-content-between'> <span>Shorts</span> <i className="bi bi-chevron-right"></i></li>
                     <li className='ctgSubTitle d-flex justify-content-between'> <span>Shirts</span> <i className="bi bi-chevron-right"></i></li>
@@ -45,9 +44,9 @@ const FilterBar = () => {
                 </datalist>
                 <span className="rangNum">$ {rangeValues[currentRangeValue]}</span>
             </div>
-            <div className='colors'>
+            <div className='colors mb-3'>
             <h4 className="filterTitle">Colors</h4>
-            <div className='colorsContent mb-2 d-flex flex-wrap'>
+            <div className='colorsContent mb-3 d-flex flex-wrap'>
                     <span className='green'></span>
                     <span className='red'></span>
                     <span className='yellow'></span>
@@ -65,28 +64,27 @@ const FilterBar = () => {
             </div>
             <h4 className="filterTitle">Size</h4>
             <div className='sizeContent mb-2 d-flex flex-wrap'>
-                    <button className='btn'>Small</button>
-                    <button className='btn'>Medium</button>
-
-                    <button className='btn'>XX-Large</button>
-                    <button className='btn'>3X-Large</button>
-                    <button className='btn'>4X-Large</button>
-                    <button className='btn'>X-Small</button>
-                    <button className='btn'>XX-Small</button>
+                    <button className='btn sizeBtn'>Small</button>
+                    <button className='btn sizeBtn'>Medium</button>
+                    <button className='btn sizeBtn'>XX-Large</button>
+                    <button className='btn sizeBtn'>3X-Large</button>
+                    <button className='btn sizeBtn'>4X-Large</button>
+                    <button className='btn sizeBtn'>X-Small</button>
+                    <button className='btn sizeBtn'>XX-Small</button>
 
             </div>
-            <div className='dressStyle'>
+            <div className='dressStyle mb-3'>
             <h4 className="filterTitle">Dress Style</h4>
 
-                <ul className='list-unstyled mb-2'>
-                    <li className='d-flex justify-content-between'><span>Tshirts</span> <i className="bi bi-chevron-right"></i></li>
-                    <li className='d-flex justify-content-between'> <span>Shorts</span> <i className="bi bi-chevron-right"></i></li>
-                    <li className='d-flex justify-content-between'> <span>Shirts</span> <i className="bi bi-chevron-right"></i></li>
-                    <li className='d-flex justify-content-between'><span>Hoodie</span> <i className="bi bi-chevron-right"></i></li>
-                    <li className='d-flex justify-content-between'><span>Jeans</span> <i className="bi bi-chevron-right"></i></li>
+                <ul className='list-unstyled mb-3 row gap-2'>
+                    <li className='d-flex ctgSubTitle justify-content-between'><span>Tshirts</span> <i className="bi bi-chevron-right"></i></li>
+                    <li className='ctgSubTitle d-flex justify-content-between'> <span>Shorts</span> <i className="bi bi-chevron-right"></i></li>
+                    <li className='d-flex ctgSubTitle justify-content-between'> <span>Shirts</span> <i className="bi bi-chevron-right"></i></li>
+                    <li className='d-flex ctgSubTitle justify-content-between'><span>Hoodie</span> <i className="bi bi-chevron-right"></i></li>
+                    <li className='d-flex ctgSubTitle justify-content-between'><span>Jeans</span> <i className="bi bi-chevron-right"></i></li>
                 </ul>
             </div>
-            <button className='btn applybtn fs-6 btn-lg px-5 py-3 me-md-2'>Apply Filter</button> 
+            <button className='btn applybtn fs-6 btn-lg px-5 py-3 me-md-2 my-3'>Apply Filter</button> 
         </div>
     )
 }
