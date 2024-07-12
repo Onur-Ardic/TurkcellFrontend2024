b4a516294624c140b0369b42f4b58a55a9aafdc4
Ebrar Nur <ebrarnurr02@gmail.com>
2024-07-12 19:46:55 +0300

    Merge branch 'main' of https://github.com/waroi/TurkcellFrontend2024

---
+++ Ogrenciler/UmutcanKocamis/Odevler/final/components/Brands.jsx
@@ -0,0 +1,13 @@
+const Brands = () => {
+  return (
+    <div className="bg-black d-flex flex-wrap align-items-center justify-content-around brands gap-3 py-4">
+      <img src="/images/versace.png" alt="" />
+      <img src="/images/zara.png" className="zara" alt="" />
+      <img src="/images/gucci.png" alt="" />
+      <img src="/images/prada.png" alt="" />
+      <img src="/images/calvin-klein.png" alt="" />
+    </div>
+  );
+};
+
+export default Brands;
---
+++ Ogrenciler/UmutcanKocamis/Odevler/final/components/BrowseByDressStyle.jsx
@@ -0,0 +1,24 @@
+
+const BrowseByDressStyle = () => {
+  return (
+    <div className="container">
+        <div className="d-flex justify-content-center text-center align-items-center flex-column gap-4 bg-secondary py-4 rounded-5">
+        <h1 className="bold">Browse By Dress Style</h1>
+        <div>
+            <img src="/images/casual-mobile.png" alt="" />
+        </div>
+        <div>
+            <img src="/images/formal-mobile.png" alt="" />
+        </div>
+        <div>
+            <img src="/images/party-mobile.png" alt="" />
+        </div>
+        <div>
+            <img src="/images/gym-mobile.png" alt="" />
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default BrowseByDressStyle
