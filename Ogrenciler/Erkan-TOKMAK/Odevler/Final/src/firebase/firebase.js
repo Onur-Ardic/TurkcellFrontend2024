3ac629833a3a8c38d248c9953b62b0326c5430b2
Erkan TOKMAK <erkan.tokmak.ofc@gmail.com>
2024-07-12 16:37:59 +0300

    env added

--- Ogrenciler/Erkan-TOKMAK/Odevler/Final/src/firebase/firebase.js
+++ Ogrenciler/Erkan-TOKMAK/Odevler/Final/src/firebase/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA_R1ejZwYmU9Zd7GEaaA03C5q7FN0k69w",
-  authDomain: "shop-co-a303e.firebaseapp.com",
-  projectId: "shop-co-a303e",
-  storageBucket: "shop-co-a303e.appspot.com",
-  messagingSenderId: "582726825897",
-  appId: "1:582726825897:web:76495e4db7704eff44f6b8"
+apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 
